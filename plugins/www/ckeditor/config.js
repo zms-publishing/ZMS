c9d70868c8db00bf959e31e9516c4abdc0b47ed4
Thorsten Fox <tf@sntl-publishing.com>
2011-04-30 10:58:51 +0000

    Added CKEditor as rich text editor
    svn path=/ZMS/trunk/; revision=1483

---
+++ plugins/www/ckeditor/config.js
@@ -0,0 +1,25 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+
+  //  Toolbar: http://docs.cksource.com/CKEditor_3.x/Developers_Guide/Toolbar
+  config.toolbar = 'ZMSBasicToolbar';
+
+  config.toolbar_ZMSBasicToolbar =
+  [
+      ['Format'],
+      ['Bold','Italic','Subscript','Superscript'],
+      ['NumberedList','BulletedList','-','Table'],
+      ['Cut','Copy','Paste','PasteText','PasteFromWord'],
+      ['Undo','Redo'],
+      ['Maximize','-','About']
+  ];
+	
+};

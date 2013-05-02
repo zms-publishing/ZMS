c8145480a49948e3261e947b45380abf4bc92d1c
zmsdev <zmsdev@localhost>
2013-05-02 19:58:52 +0000

    CKEditor: update from 3.6.6.1 to 4.1.1
    svn path=/ZMS/trunk/; revision=2022

--- plugins/www/ckeditor/config.js
+++ plugins/www/ckeditor/config.js
@@ -1,13 +1,40 @@
-/*
-Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
 
-CKEDITOR.editorConfig = function( config )
-{
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here.
+	// For the complete reference:
+	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+
+	// The toolbar groups arrangement, optimized for two toolbar rows.
+	config.toolbarGroups = [
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'forms' },
+		{ name: 'tools' },
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others' },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'about' }
+	];
+
+	// Remove some buttons, provided by the standard plugins, which we don't
+	// need to have in the Standard(s) toolbar.
+	config.removeButtons = 'Underline,Subscript,Superscript';
+
+	// Se the most common block elements.
+	config.format_tags = 'p;h1;h2;h3;pre';
+
+	// Make dialogs simpler.
+	config.removeDialogTabs = 'image:advanced;link:advanced';
 
   // Linkbutton
   // Mediabutton
@@ -15,7 +42,7 @@ CKEDITOR.editorConfig = function( config )
   config.extraPlugins = 'linkbutton,mediabutton,tableresize';
 
   // skins
-  config.skin = getZMIConfProperty("CKEDITOR.config.skin","kama");
+  //config.skin = getZMIConfProperty("CKEDITOR.config.skin","kama");
 
   //  Toolbar: @see http://docs.cksource.com/CKEditor_3.x/Developers_Guide/Toolbar
   config.toolbar = 'ZMSBasicToolbar';
@@ -27,4 +54,5 @@ CKEDITOR.editorConfig = function( config )
      ['Anchor','Table'],
      ['Source','ShowBlocks','Maximize','About']
   ];
+
 };

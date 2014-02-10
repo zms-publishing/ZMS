43ad24c5f124c9321a7452d59da9ebcbeec8c073
Dr. Frank Hoffmann <fh@sntl-publishing.com>
2014-02-10 16:42:01 +0000

    ZMS3: fixed special img attrs (req. by paritaet)
    svn path=/ZMS/branches/zms_bs3/; revision=2465

--- _objtypes.py
+++ _objtypes.py
@@ -192,7 +192,6 @@ class ObjTypes:
         imgalt = img.getFilename()
         imgtag = '<img'
         imgtag += ' src="%s"'%imgsrc
-        imgtag += ' alt="%s"'%imgalt
         imgtag += ' style="'
         if displaytype != 'export_format':
           if width != '': 
@@ -203,8 +202,10 @@ class ObjTypes:
         imgtag += '"'
         if imgclass is not None and len(imgclass) > 0:
           imgtag += ' class="%s"'%imgclass
-        if imgspecial is not None and len(imgspecial) > 0: 
+        if imgspecial is not None and len(imgspecial) > 0:
           imgtag += ' %s'%imgspecial
+        if imgspecial.find('alt=') < 0:
+          imgtag += ' alt="%s"'%imgalt
         imgtag += ' />'
         
         # Image-Url.

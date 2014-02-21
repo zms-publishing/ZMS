c027a987435670a613304b3800791a84a9feade4
zmsdev <zmsdev@localhost>
2014-02-21 22:10:38 +0000

    ZMS3: update to fancybox 2
    svn path=/ZMS/trunk/; revision=2493

--- plugins/www/jquery/plugin/jquery.plugin.extensions.js
+++ plugins/www/jquery/plugin/jquery.plugin.extensions.js
@@ -102,10 +102,10 @@ $(function(){
 
 function pluginFancybox(s, c) {
 	$.plugin('fancybox',{
-		files: ['/++resource++zms_/jquery/fancybox/jquery.easing-1.3.pack.js',
-				'/++resource++zms_/jquery/fancybox/jquery.mousewheel-3.0.4.pack.js',
-				'/++resource++zms_/jquery/fancybox/jquery.fancybox-1.3.4.pack.js',
-				'/++resource++zms_/jquery/fancybox/jquery.fancybox-1.3.4.css']
+		files: [
+				'/++resource++zms_/jquery/fancybox/jquery.mousewheel-3.0.6.pack.js',
+				'/++resource++zms_/jquery/fancybox/jquery.fancybox.pack.js',
+				'/++resource++zms_/jquery/fancybox/jquery.fancybox.css']
 		});
 	$.plugin('fancybox').get(s,c);
 }

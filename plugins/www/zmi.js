e073cfa31f8508b2e6662ca6d0453e1cd31a490d
zmsdev <zmsdev@localhost>
2014-03-05 21:33:45 +0000

    ZMS3: temporary registration of $ as jQuery-mockup
    svn path=/ZMS/trunk/; revision=2522

--- plugins/www/zmi.js
+++ plugins/www/zmi.js
@@ -3,5 +3,18 @@
  */
 ZMI = function() { this.readyFn = []};
 ZMI.prototype.registerReady = function(fn) {this.readyFn.push(fn)};
+ZMI.prototype.ready = function(fn) {this.readyFn.push(fn)};
 ZMI.prototype.runReady = function() {while (this.readyFn.length > 0) this.readyFn.pop()()};
-var $ZMI = new ZMI();
+$ZMI = new ZMI();
+/**
+ * $: Register 
+ */
+if (typeof $ == "undefined") {
+	$ = function(arg0, arg1) {
+		if (typeof arg0 == "function") {
+			$ZMI.registerReady(arg0);
+		}
+		return $ZMI;
+	}
+}
+

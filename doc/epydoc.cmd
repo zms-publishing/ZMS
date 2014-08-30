9ef298719ff08183964b02134b3da6cba4c49b2a
Dr. Frank Hoffmann <fh@sntl-publishing.com>
2014-08-30 19:36:23 +0000

    refreshed API doc
    svn path=/ZMS/trunk/; revision=2720

--- doc/epydoc.cmd
+++ doc/epydoc.cmd
@@ -1,1 +1,1 @@
-"C:\Program Files\Zope\2.11.4\Python\python.exe" "C:\Program Files\Zope\2.11.4\Python\Scripts\epydoc.py" -v --parse-only --config epydoc.conf
+"C:\dev\ZMS3\Scripts\python.exe" "C:\dev\ZMS3\Scripts\epydoc" -v --parse-only --config epydoc.conf

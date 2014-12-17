2abec44f7ad09372966912128457e378468ad4f4
Dr. Frank Hoffmann <fh@sntl-publishing.com>
2014-12-17 10:40:57 +0000

    revoke #2875 (req by unibe)
    svn path=/ZMS/trunk/; revision=2876

--- zms.py
+++ zms.py
@@ -37,6 +37,7 @@ import _enummanager
 import _fileutil
 import _globals
 import _importable
+import _mediadb
 import _objattrs
 import _xmllib
 import _zcatalogmanager
@@ -477,6 +478,13 @@ class ZMS(
     # --------------------------------------------------------------------------
     def initZMS(self, container, id, titlealt, title, lang, manage_lang, REQUEST):
       return initZMS(container, id, titlealt, title, lang, manage_lang, REQUEST)
+
+
+    # --------------------------------------------------------------------------
+    #  ZMS.manage_addMediaDb:
+    # --------------------------------------------------------------------------
+    def manage_addMediaDb(self, location, REQUEST=None, RESPONSE=None):
+      _mediadb.manage_addMediaDb(self, location, REQUEST, RESPONSE)
 
 
     # --------------------------------------------------------------------------

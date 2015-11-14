2fa68ed65fff09d88f27ea278e9d12bb6572abbc
zmsdev <zmsdev@localhost>
2015-11-14 21:03:06 +0000

    hilighted active nodes in sitemap (3)
    svn path=/ZMS/trunk/; revision=3444

--- plugins/www/bootstrap/plugin/bootstrap.plugin.zmi.js
+++ plugins/www/bootstrap/plugin/bootstrap.plugin.zmi.js
@@ -911,7 +911,6 @@ ZMIObjectTree.prototype.init = function(s,href,p) {
 			$(s).html(html);
 			var i = 0;
 			var fn = function() {
-				if (i<pages.length) {
 					var $page = $(pages[i]);
 					var page_home_id = $page.attr("home_id");
 					var page_id = $page.attr("id").substr(page_home_id.length+1);
@@ -919,10 +918,9 @@ ZMIObjectTree.prototype.init = function(s,href,p) {
 					var $toggle = $(".toggle",$ul);
 					$("li",$ul).addClass("active");
 					i++;
-					if (i<pages.length) {
+					if (pages.length==1 || i<pages.length) {
 						that.toggleClick($toggle,fn);
 					}
-				}
 			}
 			fn();
 		});

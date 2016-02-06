ecb40e7efce6ab3b52fd2df5e1006e43642bc8f0
Dr. Frank Hoffmann <fh@sntl-publishing.com>
2016-02-06 20:47:54 +0000

    ZMI: menu-tabs scrollable on small screens (2)
    svn path=/ZMS/trunk/; revision=3558

--- plugins/www/bootstrap/plugin/bootstrap.plugin.zmi.js
+++ plugins/www/bootstrap/plugin/bootstrap.plugin.zmi.js
@@ -1876,50 +1876,51 @@ $(function() {
 // ############################################################################
 // ### HORIZONTAL SCROLLING MAIN NAVIGATION FOR SMALL SCREENS
 // ############################################################################
-var hidWidth;
-var scrollBarWidths = 40;
-var widthOfList = function(){
-	var itemsWidth = 0;
-	$('.main-nav.nav-tabs li').each(function(){
-		var itemWidth = $(this).outerWidth();
-		itemsWidth+=itemWidth;
+$(function() {
+	var hidWidth;
+	var scrollBarWidths = 40;
+	var widthOfList = function(){
+		var itemsWidth = 0;
+		$('.main-nav.nav-tabs li').each(function(){
+			var itemWidth = $(this).outerWidth();
+			itemsWidth+=itemWidth;
+		});
+		return itemsWidth;
+	};
+	var widthOfHidden = function(){
+		return (($('.wrapper').outerWidth())-widthOfList()-getLeftPosi())-scrollBarWidths;
+	};
+	var getLeftPosi = function(){
+		return $('.main-nav.nav-tabs').position().left;
+	};
+	var reAdjust = function(){
+		if (($('.wrapper').outerWidth()) < widthOfList()) {
+			$('.scroller-right').show();
+		}
+		else {
+			$('.scroller-right').hide();
+		}
+	
+		if (getLeftPosi()<0) {
+			$('.scroller-left').show();
+		}
+		else {
+			$('.item').animate({left:"-="+getLeftPosi()+"px"},'slow');
+			$('.scroller-left').hide();
+		}
+	}
+	$('.scroller-right').click(function() {
+		$('.scroller-left').fadeIn('slow');
+		$('.scroller-right').fadeOut('slow');
+		$('.main-nav.nav-tabs').animate({left:"+="+widthOfHidden()+"px"},'slow',function(){ });
 	});
-	return itemsWidth;
-};
-var widthOfHidden = function(){
-	return (($('.wrapper').outerWidth())-widthOfList()-getLeftPosi())-scrollBarWidths;
-};
-var getLeftPosi = function(){
-	return $('.main-nav.nav-tabs').position().left;
-};
-var reAdjust = function(){
-	if (($('.wrapper').outerWidth()) < widthOfList()) {
-		$('.scroller-right').show();
-	}
-	else {
-		$('.scroller-right').hide();
-	}
-
-	if (getLeftPosi()<0) {
-		$('.scroller-left').show();
-	}
-	else {
-		$('.item').animate({left:"-="+getLeftPosi()+"px"},'slow');
-		$('.scroller-left').hide();
-	}
-}
-$('.scroller-right').click(function() {
-	$('.scroller-left').fadeIn('slow');
-	$('.scroller-right').fadeOut('slow');
-	$('.main-nav.nav-tabs').animate({left:"+="+widthOfHidden()+"px"},'slow',function(){ });
+	$('.scroller-left').click(function() {
+		$('.scroller-right').fadeIn('slow');
+		$('.scroller-left').fadeOut('slow');
+		$('.main-nav.nav-tabs').animate({left:"-="+getLeftPosi()+"px"},'slow',function(){ });
+	});
+	reAdjust();
+	$(window).on('resize',function(e){
+			reAdjust();
+	});
 });
-$('.scroller-left').click(function() {
-	$('.scroller-right').fadeIn('slow');
-	$('.scroller-left').fadeOut('slow');
-	$('.main-nav.nav-tabs').animate({left:"-="+getLeftPosi()+"px"},'slow',function(){ });
-});
-reAdjust();
-$(window).on('resize',function(e){
-		reAdjust();
-});
-

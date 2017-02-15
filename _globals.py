30984b61ccd0839168d95908a225b52bda6eaede
Dr. Frank Hoffmann <fh@sntl-publishing.com>
2017-02-15 11:01:24 +0000

    fixed #3972 added StringTypes import
    svn path=/ZMS/trunk/; revision=3973

--- _globals.py
+++ _globals.py
@@ -19,6 +19,9 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 ################################################################################
 
+# Imports.
+from types import StringTypes
+
 # Datatypes.
 DT_UNKNOWN = 0
 DT_BOOLEAN = 1

e082afb1286adb829dcdf2602de9dcdeaddaf914
Martin Häcker <mh@sntl-publishing.com>
2017-03-20 13:12:05 +0000

    Add ZMS interaction example
    svn path=/ZMS/trunk/; revision=4017

--- selenium_tests/example_test.py
+++ selenium_tests/example_test.py
@@ -1,13 +1,19 @@
+# encoding: utf-8
+
 import unittest
 import os
+import random
+from contextlib import contextmanager
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class SeleniumTestCase(unittest.TestCase):
+    DEFAULT_TIMEOUT = 10 # seconds
     
     @classmethod
     def setUpClass(cls):
@@ -18,7 +24,7 @@ class SeleniumTestCase(unittest.TestCase):
         
         # this ensures all find_element* methods retry up to 10 seconds for the searched 
         # element to appear in the dom. Essential if testing AJAX stuff.
-        # self.driver.implicitly_wait(10)
+        self.driver.implicitly_wait(self.DEFAULT_TIMEOUT)
         
         # self.addCleanup(self.driver.close) # doesn't work on mac?
         self.addCleanup(self.driver.quit)
@@ -33,11 +39,24 @@ class SeleniumTestCase(unittest.TestCase):
         self.driver.switch_to.alert.send_keys('admin' + Keys.TAB + 'admin')
         self.driver.switch_to.alert.accept()
     
-    def _wait(self, condition, timeout=10):
-        return WebDriverWait(self.driver, 10).until(condition)
+    def _wait(self, condition, timeout=DEFAULT_TIMEOUT):
+        return WebDriverWait(self.driver, timeout).until(condition)
     
-    def _wait_for_text(self, text, timeout=10):
+    def _wait_for_text(self, text, timeout=DEFAULT_TIMEOUT):
         return self._wait(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'body'), text), timeout=timeout)
+    
+    def _find_element(self, by, value, timeout=DEFAULT_TIMEOUT):
+        self._wait(EC.presence_of_element_located((by, value)), timeout=timeout)
+        return self.driver.find_element(by, value)
+    
+    # Modeled after http://www.obeythetestinggoat.com/how-to-get-selenium-to-wait-for-page-load-after-a-click.html
+    @contextmanager
+    def _wait_for_page_load(self, timeout=DEFAULT_TIMEOUT):
+        old_page = self.driver.find_element_by_tag_name('html')
+        yield
+        WebDriverWait(self.driver, timeout).until(
+            EC.staleness_of(old_page)
+        )
     
     def _unique_test_output_filename(self, file_name_suffix, file_type):
         assert None not in (file_name_suffix, file_type), 'need strings here'
@@ -80,6 +99,49 @@ class SeleniumTestCase(unittest.TestCase):
                     self._save_screenshot_of_current_page()
                     raise
             setattr(cls, name, wrapper)
+    
+    def _create_or_navigate_to_zms(self):
+        # expects to be logged in
+        # ensure we're on the manage_main page
+        self._wait_for_text('Control_Panel')
+        
+        if 'sites' not in self.driver.page_source:
+            # create folder
+            select = Select(self._find_element(By.CSS_SELECTOR, 'select[name=":action"]'))
+            with self._wait_for_page_load():
+                select.select_by_visible_text('Folder') # already navigates to next page
+            
+            self._find_element(By.NAME, 'id').send_keys('sites')
+            with self._wait_for_page_load():
+                self._find_element(By.CSS_SELECTOR, 'input[type="submit"][value="Add"]').click()
+        
+        with self._wait_for_page_load():
+            self._find_element(By.LINK_TEXT, 'sites').click()
+            
+        # detail page has loaded
+        if 'href="zms/manage_workspace"' not in self.driver.page_source:
+            # create zms
+            # if no link with zms is there
+            Select(self._find_element(By.CSS_SELECTOR, 'select[name=":action"]')).select_by_visible_text('ZMS')
+            with self._wait_for_page_load():
+                self._find_element(By.NAME, 'submit').click()
+        
+            # zms add page has loaded
+            self._wait_for_text('Add ZMS-Instance')
+            folder_id = self._find_element(By.ID, 'folder_id')
+            folder_id.clear()
+            folder_id.send_keys('zms')
+            with self._wait_for_page_load():
+                self._find_element(By.CSS_SELECTOR, 'button[value="Add"]').click()
+        else:
+            with self._wait_for_page_load():
+                self._find_element(By.PARTIAL_LINK_TEXT, 'zms').click()
+            with self._wait_for_page_load():
+                self._find_element(By.PARTIAL_LINK_TEXT, 'content').click()
+        
+        # zms content edit page has loaded
+        self._wait_for_text('ZMS - Python-based contentmanagement system')
+    
 
 class LoginTest(SeleniumTestCase):
     
@@ -107,7 +169,64 @@ class ScreenshotAfterFailingTest(SeleniumTestCase):
         self._login()
         self._wait_for_text('Contents')
         self.fail('Intentionally failed test')
+
+class EditPageExample(SeleniumTestCase):
+    
+    def test_edit_page(self):
+        self._login()
+        self._create_or_navigate_to_zms()
         
+        # this string will be added to the page
+        MARKER = "%s-%s" % (self.id(), random.randint(0, 100000))
+        
+        ## add textarea
+        
+        # ensure menu is visible
+        
+        def hide_zmi_actions():
+            # remove stray 'Textabschnitt' elements that linger and could catch later clicks 
+            # on buttons because they overlap them
+            self.driver.execute_script("$('.zmi-item.ZMSTextarea .zmi-action').mouseleave()")
+            
+        # seems the popup is only initialized once it is shown
+        self.driver.execute_script("$('.zmi-item.ZMSTextarea .zmi-action').mouseenter()")
+        el = self._find_element(By.CSS_SELECTOR, '.zmi-item.ZMSTextarea .zmi-action')
+        dropdown = el.find_element_by_css_selector('.dropdown-toggle')
+        self._wait(lambda driver: dropdown.is_displayed() and dropdown.is_enabled())
+        # Not sure why, but the popup only opens reliably when waiting a bit.
+        import time; time.sleep(2)
+        dropdown.click()
+        
+        hide_zmi_actions()
+        
+        create_paragraph = el.find_element_by_link_text('Textabschnitt')
+        self._wait(lambda driver: create_paragraph.is_displayed())
+        
+        hide_zmi_actions()
+        
+        create_paragraph.click()
+        
+        # wait until ckeditor is loaded
+        iframe = self.driver.find_element_by_css_selector('iframe.cke_wysiwyg_frame')
+        self.driver.switch_to.frame(iframe)
+        self.driver.find_element_by_css_selector('body').send_keys(MARKER)
+        
+        # click insert
+        self.driver.switch_to.default_content()
+        hide_zmi_actions()
+        self._find_element(By.XPATH, '//button[text()="Einfügen"]').click()
+        
+        # wait until saved
+        self.driver.find_element_by_css_selector('.alert-success')
+        
+        # open preview
+        with self._wait_for_page_load():
+            self.driver.find_element_by_link_text("Vorschau").click()
+        frame = self.driver.find_element_by_name("partner")
+        self.driver.switch_to.frame(frame)
+        
+        # ensure text is there
+        self.driver.find_element_by_xpath('//p[text()="%s"]' % MARKER)
 
 if __name__ == "__main__":
     unittest.main()

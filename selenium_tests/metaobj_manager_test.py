87161ab11826024905a5219301eb9461c3395fa6
zmsdev <zmsdev@localhost>
2017-04-22 22:11:01 +0000

    AC-Tests: metaobj-manager
    svn path=/ZMS/trunk/; revision=4055

---
+++ selenium_tests/metaobj_manager_test.py
@@ -0,0 +1,36 @@
+# encoding: utf-8
+
+import unittest
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import example_test
+
+# python -m unittest selenium_tests.metaobj_manager_test.MetaobjManagerTest
+class MetaobjManagerTest(example_test.SeleniumTestCase):
+   
+      def test_import_conf(self):
+       
+        self._login()
+        self._create_or_navigate_to_zms()
+        self.driver.get(self.driver.current_url)
+       
+        # open config
+        navbar = self._wait_for_element('.navbar ul.nav.navbar-nav')
+        navbar_item = navbar.find_element_by_link_text('Content-Objekte')
+        navbar_item.click()
+       
+        # wait until opened
+        self._find_element(By.CSS_SELECTOR, 'body.metas.config')
+       
+        time.sleep(5)
+        print "Done"
+
+
+if __name__ == "__main__":
+    unittest.main()

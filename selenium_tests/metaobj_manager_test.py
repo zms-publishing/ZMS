61cff3973800f3e398a7fb9345532f80bf06c786
zmsdev <zmsdev@localhost>
2017-04-24 19:45:58 +0000

    AC-Tests: metaobj-manager
    svn path=/ZMS/trunk/; revision=4061

--- selenium_tests/metaobj_manager_test.py
+++ selenium_tests/metaobj_manager_test.py
@@ -14,21 +14,53 @@ import example_test
 # python -m unittest selenium_tests.metaobj_manager_test.MetaobjManagerTest
 class MetaobjManagerTest(example_test.SeleniumTestCase):
    
-      def test_import_conf(self):
+      def test_metaobj_conf(self):
        
         self._login()
         self._create_or_navigate_to_zms()
         self.driver.get(self.driver.current_url)
-       
+        
         # open config
-        self._find_element(By.CSS_SELECTOR, 'a[title="Konfiguration"]').click()
         navbar = self._wait_for_element('.navbar-main')
-        navbar_item = navbar.find_element_by_link_text('Content-Objekte')
-        navbar_item.click()
-       
+        navbar.find_element_by_css_selector('.dropdown-toggle').click()
+        navbar.find_element_by_link_text('Content-Objekte').click()
+        
         # wait until opened
         self._find_element(By.CSS_SELECTOR, 'body.metas.config')
+        
+        # open import dialog
+        self._find_element(By.CSS_SELECTOR, '.btn.btn-default[title="Importieren..."]').click()
+        dialog = self._wait_for_element('iframe')
+        self.driver.switch_to.frame(dialog)
+        self.driver.execute_script("$('select#init').mouseenter()")
+        time.sleep(2)
+        dialog.find_element_by_css_selector('.btn.btn-primary[value="Importieren"]').click()
+        
+        time.sleep(5)
+        print "Done"
+
+
+      def test_metadict_conf(self):
        
+        self._login()
+        self._create_or_navigate_to_zms()
+        self.driver.get(self.driver.current_url)
+        
+        # open config
+        navbar = self._wait_for_element('.navbar-main')
+        navbar.find_element_by_css_selector('.dropdown-toggle').click()
+        navbar.find_element_by_link_text('Meta-Attribute').click()
+        
+        # wait until opened
+        self._find_element(By.CSS_SELECTOR, 'body.metas.config')
+        
+        # open import dialog
+        self._find_element(By.CSS_SELECTOR, '.btn.btn-default[title="Importieren..."]').click()
+        dialog = self._wait_for_element('.modal-dialog')
+        self.driver.execute_script("$('select#init').mouseenter()")
+        time.sleep(2)
+        dialog.find_element_by_css_selector('.btn.btn-primary[value="Importieren"]').click()
+        
         time.sleep(5)
         print "Done"
 

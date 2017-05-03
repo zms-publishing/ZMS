30fe9fa6275220624a81128c64089df934da44b9
zmsdev <zmsdev@localhost>
2017-05-03 19:35:42 +0000

    AC-Tests: add/delete content-language
    svn path=/ZMS/trunk/; revision=4070

--- selenium_tests/multilang_manager_test.py
+++ selenium_tests/multilang_manager_test.py
@@ -42,7 +42,7 @@ class MultilangManagerTest(example_test.SeleniumTestCase):
         
         # open delete dialog
         self._find_element(By.CSS_SELECTOR, '#changeLanguagesForm input[name="ids:list"][value="eng"]').click()
-        self._find_element(By.CSS_SELECTOR, '#changeLanguagesForm .btn.btn-default[title="Löschen..."]').click()
+        self._find_element(By.CSS_SELECTOR, '#changeLanguagesForm .btn.btn-default[value="Löschen..."]').click()
         time.sleep(1)
         self.driver.switch_to_alert().accept()
         

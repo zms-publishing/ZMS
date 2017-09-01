8d30874f0a5dfd2d6c794c6a0cb200c8ec8fa28b
zmsdev <zmsdev@localhost>
2017-09-01 16:28:58 +0000

    AC-Tests
    svn path=/ZMS/trunk/; revision=4153

--- selenium_tests/metaobj_manager_test.py
+++ selenium_tests/metaobj_manager_test.py
@@ -23,36 +23,37 @@ class MetaobjManagerTest(example_test.SeleniumTestCase):
         # open config
         navbar = self._find_element(By.CSS_SELECTOR, '.navbar-main')
         navbar.find_element_by_css_selector('.dropdown-toggle').click()
-        content_objects = navbar.find_element_by_link_text('Content-Objekte')
-        self._wait(lambda driver: content_objects.is_displayed())
+        navbar = navbar.find_element_by_link_text('Content-Objekte')
+        self._wait(lambda driver: navbar.is_displayed())
         with self._wait_for_page_load():
-            content_objects.click()
+            navbar.click()
         
         # wait until opened
         self._find_element(By.CSS_SELECTOR, 'body.metas.config')
         
         # open insert dialog
-        self._find_element(By.CSS_SELECTOR, '.btn.btn-primary[title="Einfügen..."]').click()
-        
+        self._find_element(By.CSS_SELECTOR, '.btn[title="Einfügen..."]').click()
         dialog = self._find_element(By.CSS_SELECTOR, '#zmiModalinsertObj')
+        self._wait(lambda driver: dialog.is_displayed())
         dialog.find_element(By.CSS_SELECTOR, 'input[name="_meta_id"]').send_keys('LgTest')
         dialog.find_element(By.CSS_SELECTOR, 'input[name="_meta_name"]').send_keys('Test')
-        dialog.find_element(By.CSS_SELECTOR, '.btn.btn-primary[value="Einfügen"]').click()
+        with self._wait_for_page_load():
+            dialog.find_element(By.CSS_SELECTOR, '.btn[value="Einfügen"]').click()
         
         # wait until saved
         self._find_element(By.CSS_SELECTOR, '.alert-success')
         
-        # open config (also removes .alert-success)
-        navtabs = self._find_element(By.CSS_SELECTOR, '.nav.nav-tabs')
-        with self._wait_for_page_load(timeout=5):
-            navtabs.find_element_by_link_text('Content-Objekte').click()
+        # reload page
+        with self._wait_for_page_load():
+            self._wait_for_element('.nav.nav-tabs .active').click()
         
         # open delete dialog
         self._find_element(By.CSS_SELECTOR, 'input[name="ids:list"][value="LgTest"]').click()
-        self._find_element(By.CSS_SELECTOR, '.btn.btn-default[title="Löschen..."]').click()
+        self._find_element(By.CSS_SELECTOR, '.btn[title="Löschen..."]').click()
         wait = WebDriverWait(self.driver, 5)
         wait.until(EC.alert_is_present())
-        self.driver.switch_to_alert().accept()
+        with self._wait_for_page_load():
+            self.driver.switch_to_alert().accept()
         
         # wait until saved
         self._find_element(By.CSS_SELECTOR, '.alert-success')

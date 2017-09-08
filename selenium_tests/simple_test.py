5687ec103757a6cbb500e1d359a117e2bc11516f
zmsdev <zmsdev@localhost>
2017-09-08 15:30:27 +0000

    AC-Tests
    svn path=/ZMS/trunk/; revision=4188

--- selenium_tests/simple_test.py
+++ selenium_tests/simple_test.py
@@ -35,7 +35,7 @@ class EditDocTest(example_test.SeleniumTestCase):
         item.click()
         
         # click create document
-        self._find_element(By.CSS_SELECTOR, '#'+id+' .zmi-action .dropdown-menu')
+        #self._find_element(By.CSS_SELECTOR, '#'+id+' .zmi-action .dropdown-menu')
         item = el.find_element_by_link_text('Dokument')
         self._wait(lambda driver: item.is_displayed())
         item.click()
@@ -85,7 +85,7 @@ class EditDocTest(example_test.SeleniumTestCase):
         item.click()
        
         # click delete document
-        self._find_element(By.CSS_SELECTOR, '#'+id+' .zmi-action .dropdown-menu')
+        #self._find_element(By.CSS_SELECTOR, '#'+id+' .zmi-action .dropdown-menu')
         item = el.find_element_by_css_selector('.icon-trash')
         self._wait(lambda driver: item.is_displayed())
         item.click()

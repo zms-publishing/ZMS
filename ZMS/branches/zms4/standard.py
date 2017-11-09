ee394c8455558381ed5f08d026cca93901eb86ef
zmsdev <zmsdev@37c01b7e-e2c2-4650-95c3-06c4adb181d6>
2017-11-09 18:03:58 +0000

    fix ZMI: moved /misc_/zms to /++resource++zms_/img

--- ZMS/branches/zms4/standard.py
+++ ZMS/branches/zms4/standard.py
@@ -464,7 +464,7 @@ def getMimeTypeIconSrc(mt):
   @type mt: C{str}
   @rtype: C{str}
   """
-  return'/misc_/zms/%s'%_mimetypes.dctMimeType.get( mt, _mimetypes.content_unknown)
+  return'/++resource++zms_/img/%s'%_mimetypes.dctMimeType.get( mt, _mimetypes.content_unknown)
 
 
 security.declarePublic('unencode')

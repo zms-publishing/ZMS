c16b1365e83d8e58664cd8b23329e4b2e3661af2
zmsdev <zmsdev@localhost>
2017-11-19 19:57:10 +0000

    removed fix for form-enctype (back to multipart/form-data)
    svn path=/ZMS/branches/zms4/; revision=4321

--- plugins/www/ckeditor/plugins/mediabutton/dialogs/link.js
+++ plugins/www/ckeditor/plugins/mediabutton/dialogs/link.js
@@ -44,7 +44,7 @@ CKEDITOR.dialog.add( 'mediabuttonDlg', function( editor )
 						html : ''
 							+ '<div id="mediaDialog">'
 								+ '<iframe id="CKEditor_uploadMedia" name="CKEditor_uploadMedia" stc="" style="display:none" onload="CKEditor_uploadMediaDone()"></iframe>'
-								+ '<form action="metaobj_manager/ZMSLib.uploadMedia" method="post" target="CKEditor_uploadMedia" enctype="multipart/form-data;charset=utf-8">'
+								+ '<form action="metaobj_manager/ZMSLib.uploadMedia" method="post" target="CKEditor_uploadMedia" enctype="multipart/form-data">'
 								  + '<input type="hidden" name="session_id" value="'+$("form#form0 input[name=session_id]").val()+'"/>'
 								  + '<input type="hidden" name="form_id" value="'+$("form#form0 input[name=form_id]").val()+'"/>'
 								  + '<input type="hidden" name="lang" value="'+$("form#form0 input[name=lang]").val()+'"/>'

1509165e989caa0997106408de192b8997c9df33
Dr. Frank Hoffmann <fh@sntl-publishing.com>
2018-03-26 04:39:17 +0000

    fixed MacOS folder handling (contrib. by CMeier)
    svn path=/ZMS/trunk/; revision=4487

--- ZMSRepositoryManager.py
+++ ZMSRepositoryManager.py
@@ -183,6 +183,8 @@ class ZMSRepositoryManager(
               temp_files = \
                 map(lambda x:[x[0],x[-1].replace('.py','')],filter(lambda x:len(x)==2,temp_files)) + \
                 map(lambda x:[x[0],x[-2]],filter(lambda x:len(x)>2,temp_files))
+              # avoid processing of hidden files, e.g. .DS_Store on macOS
+              temp_files = filter(lambda x: x.startswith('.'), temp_files)
               ids = list(set(map(lambda x:':'.join(x),temp_files)))
               self.writeBlock("[exec_auto_update]: %s"%str(ids))
               self.updateChanges(ids, override=True)

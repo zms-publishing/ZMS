27393106ee5d26014c5c2aa5c0780b0e85fae0ca
zmsdev <zmsdev@localhost>
2018-05-16 14:54:42 +0000

    removed zms3 tests
    svn path=/ZMS/branches/zms4/; revision=4535

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-import test_util
-import test_zreferableitem
-import test_zmscontainerobject
-import test_zmslinkelement
-import test_multilang
-import test_performance
-import test_pilutil
-import test_standard
-import test_xmllib

95c70b004f9e00da8de38a3d1942548f8f1e6724
zmsdev <zmsdev@localhost>
2019-10-26 01:13:34 +0000

    multiple ace-editors
    svn path=/ZMS/branches/zms4/; revision=5283

--- Products/zms/plugins/www/common/zmi_ace_editor.js
+++ Products/zms/plugins/www/common/zmi_ace_editor.js
@@ -18,60 +18,64 @@ $(function(){
 			}
 		});
 		// @see https://github.com/ajaxorg/ace/wiki/Embedding---API
-		var $textarea = $("textarea",this).hide();
-		var name = $textarea.attr("name");
-		var value = $textarea.val();
-		var editor = ace.edit("editor_"+name);
-		var content_type = $("input#content_type").val();
-		if (typeof content_type == "undefined" || content_type == null || content_type == '' || content_type == 'text/x-unknown-content-type') {
-			var absolute_url = $("span#absolute_url").text();
-			var id = absolute_url.substr(absolute_url.lastIndexOf("/")+1);
-			if (id.endsWith(".css")) {
-				content_type = "text/css";
+		var $textarea = $(".form-control-ace-editor",this);
+		if ($textarea.is(":visible")) {
+			$textarea.hide();
+			var name = $textarea.attr("name");
+			var value = $textarea.val();
+			var editor = ace.edit("editor_"+name);
+			var content_type = $("input#content_type",this).val();
+			if (typeof content_type == "undefined" || content_type == null || content_type == '' || content_type == 'text/x-unknown-content-type') {
+				var absolute_url = $(".control-label-ace-editor a",this).attr("href");
+				absolute_url = absolute_url.substr(0,absolute_url.lastIndexOf("/")); // strip manage_main
+				var id = absolute_url.substr(absolute_url.lastIndexOf("/")+1);
+				if (id.endsWith(".css")) {
+					content_type = "text/css";
+				}
+				else if (id.endsWith(".less")) {
+					content_type = "text/less";
+				}
+				else if (id.endsWith(".js")) {
+					content_type = "text/javascript";
+				}
+				else {
+					content_type = "text/html";
+				}
 			}
-			else if (id.endsWith(".less")) {
-				content_type = "text/less";
-			}
-			else if (id.endsWith(".js")) {
-				content_type = "text/javascript";
-			}
-			else {
+			if (value.indexOf("<html") == 0) {
 				content_type = "text/html";
 			}
+			if (value.indexOf("<?xml") == 0 || value.indexOf("tal:") >= 0) {
+				content_type = "text/xml";
+			}
+			if (value.indexOf("#!/usr/bin/python") == 0 || value.indexOf("## Script (Python)") == 0) {
+				content_type = "python";
+			}
+			var mode = "text";
+			if (content_type == "text/html") {
+				mode = "html";
+			}
+			else if (content_type == "text/css" || content_type == "application/css" || content_type == "application/x-css") {
+				mode = "css";
+			}
+			else if (content_type == "text/less") {
+				mode = "less";
+			}
+			else if (content_type == "text/javascript" || content_type == "application/javascript" || content_type == "application/x-javascript") {
+				mode = "javascript";
+			}
+			else if (content_type == "text/xml") {
+				mode = "xml";
+			}
+			else if (content_type == "python") {
+				mode = 'python';
+			}
+			editor.setTheme("ace/theme/chrome");
+			editor.getSession().setMode('ace/mode/'+mode);
+			editor.getSession().setValue(value);
+			editor.getSession().on("change",function() {
+				$textarea.val(editor.getSession().getValue()).change();
+			});
 		}
-		if (value.indexOf("<html") == 0) {
-			content_type = "text/html";
-		}
-		if (value.indexOf("<?xml") == 0 || value.indexOf("tal:") >= 0) {
-			content_type = "text/xml";
-		}
-		if (value.indexOf("#!/usr/bin/python") == 0 || value.indexOf("## Script (Python)") == 0) {
-			content_type = "python";
-		}
-		var mode = "text";
-		if (content_type == "text/html") {
-			mode = "html";
-		}
-		else if (content_type == "text/css" || content_type == "application/css" || content_type == "application/x-css") {
-			mode = "css";
-		}
-		else if (content_type == "text/less") {
-			mode = "less";
-		}
-		else if (content_type == "text/javascript" || content_type == "application/javascript" || content_type == "application/x-javascript") {
-			mode = "javascript";
-		}
-		else if (content_type == "text/xml") {
-			mode = "xml";
-		}
-		else if (content_type == "python") {
-			mode = 'python';
-		}
-		editor.setTheme("ace/theme/chrome");
-		editor.getSession().setMode('ace/mode/'+mode);
-		editor.getSession().setValue(value);
-		editor.getSession().on("change",function() {
-			$textarea.val(editor.getSession().getValue()).change();
-		});
 	});
 });

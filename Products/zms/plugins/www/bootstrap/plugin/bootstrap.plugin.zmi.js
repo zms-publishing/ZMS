8679fa3e830b51db281f9f07a9a9cb707f1c31ee
Dr. Frank Hoffmann <fh@sntl-publishing.com>
2019-10-31 14:44:07 +0000

    removed js comments
    svn path=/ZMS/branches/zms4/; revision=5297

--- Products/zms/plugins/www/bootstrap/plugin/bootstrap.plugin.zmi.js
+++ Products/zms/plugins/www/bootstrap/plugin/bootstrap.plugin.zmi.js
@@ -2177,10 +2177,7 @@ $(function() {
 	// Reference: https://getflywheel.com/layout/sticky-back-to-top-button-tutorial/
 	const scrollToTopButton = document.getElementById('js-top');
 	const scrollFunc = () => {
-		// Get the current scroll value
 		let y = window.scrollY;
-
-		// If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
 		if (y > 0) {
 			scrollToTopButton.className = "back-to-top show";
 		} else {
@@ -2189,20 +2186,12 @@ $(function() {
 	};
 	window.addEventListener("scroll", scrollFunc);
 	const scrollToTop = () => {
-		// Let's set a variable for the number of pixels we are from the top of the document.
 		const c = document.documentElement.scrollTop || document.body.scrollTop;
-
-		// If that number is greater than 0, we'll scroll back to 0, or the top of the document.
-		// We'll also animate that scroll with requestAnimationFrame:
-		// https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
 		if (c > 0) {
 			window.requestAnimationFrame(scrollToTop);
-			// ScrollTo takes an x and a y coordinate.
-			// Increase the '10' value to get a smoother/slower scroll!
 			window.scrollTo(0, c - c / 10);
 		}
 	}
-	// When the button is clicked, run our ScrolltoTop function above!
 	scrollToTopButton.onclick = function(e) {
 		e.preventDefault();
 		scrollToTop();

b3e4397dd1dc7b3cb970ee7453e805bc224124f9
Dr. Frank Hoffmann <fh@sntl-publishing.com>
2019-10-31 14:22:11 +0000

    fixed scroll-to-top button
    svn path=/ZMS/branches/zms4/; revision=5296

--- Products/zms/plugins/www/bootstrap/plugin/bootstrap.plugin.zmi.js
+++ Products/zms/plugins/www/bootstrap/plugin/bootstrap.plugin.zmi.js
@@ -2174,21 +2174,39 @@ $(function() {
 // ### BACK-TO-TOP-SCROLL BUTTON
 // ############################################################################
 $(function() {
-	var offset = 150;
-	var duration = 300;
-	$(window).scroll(function() {
-		if ($(this).scrollTop() > offset) {
-			$('.back-to-top').fadeIn(duration);
-			} else {
-			$('.back-to-top').fadeOut(duration);
-			}
-	});
+	// Reference: https://getflywheel.com/layout/sticky-back-to-top-button-tutorial/
+	const scrollToTopButton = document.getElementById('js-top');
+	const scrollFunc = () => {
+		// Get the current scroll value
+		let y = window.scrollY;
 
-	$('.back-to-top').click(function(event) {
-		event.preventDefault();
-		$('html, body').animate({scrollTop: 0}, duration);
-		return false;
-	});
+		// If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+		if (y > 0) {
+			scrollToTopButton.className = "back-to-top show";
+		} else {
+			scrollToTopButton.className = "back-to-top hide";
+		}
+	};
+	window.addEventListener("scroll", scrollFunc);
+	const scrollToTop = () => {
+		// Let's set a variable for the number of pixels we are from the top of the document.
+		const c = document.documentElement.scrollTop || document.body.scrollTop;
+
+		// If that number is greater than 0, we'll scroll back to 0, or the top of the document.
+		// We'll also animate that scroll with requestAnimationFrame:
+		// https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+		if (c > 0) {
+			window.requestAnimationFrame(scrollToTop);
+			// ScrollTo takes an x and a y coordinate.
+			// Increase the '10' value to get a smoother/slower scroll!
+			window.scrollTo(0, c - c / 10);
+		}
+	}
+	// When the button is clicked, run our ScrolltoTop function above!
+	scrollToTopButton.onclick = function(e) {
+		e.preventDefault();
+		scrollToTop();
+	}
 });
 
 // /////////////////////////////////////////////////////////////////////////////

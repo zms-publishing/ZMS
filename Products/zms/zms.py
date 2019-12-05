f9f21ed8adbaa88a61e77d7ba2d7de02f3409fbd
zmsdev <zmsdev@localhost>
2019-12-05 20:44:29 +0000

    imports from pip install
    svn path=/ZMS/branches/zms4/; revision=5345

--- Products/zms/zms.py
+++ Products/zms/zms.py
@@ -62,16 +62,9 @@ __all__= ['ZMS']
 ################################################################################
 
 import zope.event
-try:
-# module path of a pip based installation
-  from zope.container.contained import ObjectAddedEvent
-  from zope.container.contained import ObjectMovedEvent
-  from zope.container.contained import ObjectRemovedEvent
-except:
-# module path of an egg based installation
-  from zope.app.container.contained import ObjectAddedEvent
-  from zope.app.container.contained import ObjectMovedEvent
-  from zope.app.container.contained import ObjectRemovedEvent
+from zope.container.contained import ObjectAddedEvent
+from zope.container.contained import ObjectMovedEvent
+from zope.container.contained import ObjectRemovedEvent
 
 def subscriber(event):
   if isinstance(event, ObjectAddedEvent):

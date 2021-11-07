f071b64ffc0e62db3cd7a812f968c1249087e710
zmsdev <zmsdev@sntl-publishing.com>
2021-11-07 16:44:31 +0100

    zmssqldb: fixed zmi details-grid

--- Products/zms/zmssqldb.py
+++ Products/zms/zmssqldb.py
@@ -486,8 +486,7 @@ class ZMSSqlDb(zmscustom.ZMSCustom):
               standard.writeError( self, '[getEntityRecordHandler]: can\'t %s'%k)
             d[k] = value
           primary_key = context.getEntityPK(tableName)
-          rowid = standard.operator_getitem(d, primary_key, ignorecase=True)
-          # rowid  = r.get(primary_key)
+          rowid = standard.operator_getitem(r, primary_key, ignorecase=True)
           d['__id__'] = rowid
           d['params'] = {'rowid':rowid}
           return d

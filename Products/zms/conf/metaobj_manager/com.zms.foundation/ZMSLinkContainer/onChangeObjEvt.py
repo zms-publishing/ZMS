8f1cb76db904f1559b47373d4d0d500041e0c82e
drfho <dr.frank.hoffmann@web.de>
2021-11-25 23:32:32 +0100

    fixed adding ZMSLinkElement (UniBE #357)

--- Products/zms/conf/metaobj_manager/com.zms.foundation/ZMSLinkContainer/onChangeObjEvt.py
+++ Products/zms/conf/metaobj_manager/com.zms.foundation/ZMSLinkContainer/onChangeObjEvt.py
@@ -8,6 +8,7 @@
 ##title=py: Event: onChangeObj
 ##
 # --// BO onChangeObjEvt //--
+from Products.zms import standard
 
 def equals(o, d):
   for k in d:
@@ -42,6 +43,6 @@ if request.get('btn') == 'BTN_SAVE':
     description = request['description%s'%id].strip()
     attr_type = ['new','replace'][int(url.startswith('{$') and url.endswith('}'))]
     d = {'active':active,'title':title,'titlealt':title,'attr_ref':url,'attr_dc_description':description,'attr_type':attr_type,'align':align}
-    childNode = zmscontext.manage_addZMSCustom('ZMSLinkElement',d,request)
+    childNode = standard.addZMSCustom(self=zmscontext, meta_id='ZMSLinkElement', values=d, REQUEST=request)
 
 # --// EO onChangeObjEvt //--

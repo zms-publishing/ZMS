37e90904ecf2097067c39ba87574747e7c717b1e
drfho <dr.frank.hoffmann@web.de>
2022-01-28 17:48:09 +0100

    zmsindex manage tab: monotonized gui

--- Products/zms/conf/metacmd_manager/manage_tab_ZMSIndexZCatalog/manage_tab_ZMSIndexZCatalog.py
+++ Products/zms/conf/metacmd_manager/manage_tab_ZMSIndexZCatalog/manage_tab_ZMSIndexZCatalog.py
@@ -21,7 +21,7 @@ def manage_tab_ZMSIndexZCatalog( self):
   
   uid = request.form.get('uid','')
   prt.append('<div class="form-group zms4-row">')
-  prt.append('<label class="col-sm-2 control-label">Uid</label>')
+  prt.append('<label class="col-sm-2 control-label">UID</label>')
   prt.append('<div class="col-sm-5">')
   prt.append('<div class="input-group" title="SEARCH for a given uid and show the index details.">')
   prt.append('<input class="form-control" type="text" name="uid" value="%s" placeholder="Enter uid: xxx-yyy-zzz"/>'%uid)
@@ -52,13 +52,13 @@ def manage_tab_ZMSIndexZCatalog( self):
   prt.append('<div class="form-group zms4-row">')
   prt.append('<label class="col-sm-2 control-label">Path</label>')
   prt.append('<div class="col-sm-5">')
-  prt.append('<div class="input-group" title="SEARCH for a given path and show the index details.">')
+  prt.append('<div class="input-group">')
   prt.append('<input class="form-control" type="text" name="path" value="%s" placeholder="Enter path /xxx/yyy/zzz"/>'%path)
   prt.append('<div class="input-group-append input-group-btn">')
-  prt.append('<button class="btn btn-primary" name="btn" value="search">')
+  prt.append('<button class="btn btn-primary" name="btn" value="search" title="SEARCH for a given path and show the index details.">')
   prt.append('<i class="icon-search fas fa-search"></i>')
   prt.append('</button>')
-  prt.append('<button class="btn btn-danger" name="btn" value="uncatalog">')
+  prt.append('<button class="btn btn-danger" name="btn" value="uncatalog" title="DELETE all items from index for a given path.">')
   prt.append('<i class="icon-search fas fa-times"></i>')
   prt.append('</button>')
   prt.append('</div>')
@@ -71,13 +71,14 @@ def manage_tab_ZMSIndexZCatalog( self):
     prt.append('<label class="col-sm-2 control-label">&nbsp;</label>')
     prt.append('<div class="col-sm-10">')
     prt.append('<strong>%s: %i</strong>'%(self.getZMILangStr('SEARCH_RETURNEDRESULTS'),len(q)))
-    prt.append('<ul>')
+    prt.append('<ol>')
     for r in q:
-      prt.append('<li>')
-      prt.append('<a href="%s/manage" target="_blank">%s</a>'%(r['getPath'],r['getPath']))
       if btn == 'uncatalog':
         catalog.uncatalog_object(r['getPath'])
-        prt.append('<i class="fas fa-times"></i>')
+        prt.append('<li class="uncataloged">')
+      else:
+        prt.append('<li class="cataloged">')
+      prt.append('<a href="%s/manage" target="_blank">%s</a>'%(r['getPath'],r['getPath']))
       prt.append('</li>')
 
     prt.append('</ul>')
@@ -88,6 +89,33 @@ def manage_tab_ZMSIndexZCatalog( self):
   prt.append('</form><!-- .form-horizontal -->')
   prt.append('</div><!-- #zmi-tab -->')
   prt.append(self.zmi_body_footer(self,request))
+  prt.append('''
+    <style>
+    li.uncataloged a:before,
+    li.cataloged a:before {
+      font-family: 'Font Awesome 5 Free';
+      font-weight: 900;
+      -moz-osx-font-smoothing: grayscale;
+      -webkit-font-smoothing: antialiased;
+      display: inline-block;
+      font-style: normal;
+      font-variant: normal;
+      text-rendering: auto;
+      line-height: 1;
+      margin-right: .25rem;
+    }
+    li.cataloged a:before {
+      content: "\\f058";
+      color: #286090;
+    }
+    li.uncataloged a {
+        color:#dc3545 !important;
+    }
+    li.uncataloged a:before {
+      content: "\\f057";
+    }
+    <style>
+  ''')
   prt.append('</body>')
   prt.append('</html>')
   

6d46cf21bb6ad10c8f3b36f2dcf5834efc091c7d
zmsbot <zmsbot@zms.hosting>
2022-03-10 16:11:05 +0100

    http_import: fixed query encoding

--- Products/zms/standard.py
+++ Products/zms/standard.py
@@ -873,7 +873,8 @@ def http_import(context, url, method='GET', auth=None, parse_qs=0, timeout=10, h
   scheme = u[0]
   netloc = u[1]
   path = u[2]
-  query = u[4].encode('utf-8')
+  # query = u[4].encode('utf-8') ## REVOKE SOLR https://github.com/zms-publishing/ZMS/commit/49e0a89b8b1f21ea373354d231e6efa42f51b27f#diff-ca03d9475057635974de854d31d24453143a9f2ad23fa25454d1f371a584a002L814
+  query = u[4]
 
   # Get Proxy.
   useproxy = True

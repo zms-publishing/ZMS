604026b79cab6cfa93202235883bc2cefb20a1b4
drfho <dr.frank.hoffmann@web.de>
2022-03-11 22:01:16 +0100

    refactored search/replace action (RBZ)

---
+++ Products/zms/conf/metacmd_manager/manage_searchReplace/__init__.py
@@ -0,0 +1,51 @@
+class manage_searchReplace:
+	"""
+	python-representation of manage_searchReplace
+	"""
+
+	# Acquired
+	acquired = 0
+
+	# Action
+	action = "%smanage_executeMetacmd?id=manage_searchReplace"
+
+	# Description
+	description = ""
+
+	# Execution
+	execution = False
+
+	# Icon_clazz
+	icon_clazz = "fas fa-random"
+
+	# Id
+	id = "manage_searchReplace"
+
+	# Meta_types
+	meta_types = ["type(ZMSDocument)"
+		,"type(ZMSObject)"]
+
+	# Name
+	name = "Search+Replace..."
+
+	# Nodes
+	nodes = "{$}"
+
+	# Revision
+	revision = "5.0.0"
+
+	# Roles
+	roles = ["ZMSAdministrator"
+		,"ZMSEditor"
+		,"ZMSAuthor"]
+
+	# Stereotype
+	stereotype = ""
+
+	# Title
+	title = "Search and Replace..."
+
+	# Impl
+	class Impl:
+		manage_searchreplace = {"id":"manage_searchReplace"
+			,"type":"Script (Python)"}
---
+++ Products/zms/conf/metacmd_manager/manage_searchReplace/manage_searchReplace.py
@@ -0,0 +1,117 @@
+## Script (Python) "manage_searchReplace"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+from Products.zms import standard
+request = container.REQUEST
+RESPONSE = request.RESPONSE
+html=[]
+
+def replace(o, old, new):
+	if isinstance(o,(str,bytes)):
+		return o.replace(old,new)
+	elif standard.operator_gettype(o) is standard.operator_gettype({}):
+		for k in o:
+			o[k] = replace(o[k],old,new)
+	elif standard.operator_gettype(o) is standard.operator_gettype([]):
+		o = [replace(x,old,new) for x in o]
+	return o
+
+def run(here, old, new):
+	# import pdb; pdb.set_trace()
+	rtn = []
+	for key in here.getObjAttrs().keys():
+		objAttr = here.getObjAttr(key)
+		langIds = ['']
+		if objAttr['multilang']:
+			langIds = here.getLangIds()
+		for objVers in here.getObjVersions():
+			for langId in langIds:
+				objAttrName = here.getObjAttrName(objAttr,langId)
+				objAttrVal = here.operator_getattr(objVers,objAttrName,None)
+				if objAttrVal:
+					newVal = replace(objAttrVal,old,new)
+					if newVal != objAttrVal:
+						# Do only replacing if checkbox 'replace' was clicked
+						if request.get('replace'):
+							here.operator_setattr(objVers,objAttrName,objAttrVal.replace(old,new))
+						rtn.append(here)
+	for childNode in here.getChildNodes():
+		rtn.extend(run(childNode,old,new))
+	return rtn
+
+html.append('<!DOCTYPE html>')
+html.append('<html lang="en">')
+html.append(context.zmi_html_head(context,request))
+html.append('<body class="%s">'%(' '.join(['zmi',request['lang'],'transition',context.meta_id])))
+html.append(context.zmi_body_header(context,request,options=[{'action':'#','label':'Search+Replace...'}]))
+html.append('<div id="zmi-tab">')
+html.append(context.zmi_breadcrumbs(context,request))
+html.append('<form class="form-horizontal card" method="post" enctype="multipart/form-data">')
+html.append('<input type="hidden" name="form_id" value="manage_addClient"/>')
+html.append('<input type="hidden" name="lang" value="%s"/>'%request['lang'])
+html.append('<legend>Search+Replace...</legend>')
+html.append('<div class="card-body">')
+
+# --- Execute.
+# ---------------------------------
+if request.form.get('btn')=='BTN_EXECUTE':
+	message = []
+	old = request['old']
+	new = request['new']
+	res = run(context,old,new)
+	if str(request.form.get('replace'))=='1':
+		message.append('%s Results found for <i>%s</i> and changed to <i>%s</i>.'%(len(res),old,new))
+	else:
+		message.append('%s Results found for <i>%s</i> and NOT changed.'%(len(res),old))
+	message.append('<ol>')
+	message.extend(['<li><a href="%s/manage_main" target="_blank">%s</a></li>'%(x.absolute_url(),x.absolute_url()) for x in res])
+	message.append('</ol>')
+	request.response.redirect(context.url_append_params('manage_main',{'lang':request['lang'],'manage_tabs_message':'\n'.join(message)}))
+
+# --- Display initial insert form.
+# ---------------------------------
+else:
+	btn_text_exec = standard.pystr(context.getZMILangStr('BTN_EXECUTE'), encoding='utf-8', errors='replace')
+	btn_text_cncl = standard.pystr(context.getZMILangStr('BTN_CANCEL'), encoding='utf-8', errors='replace')
+	html.append('''
+		<div class="form-group row">
+			<label for="old" class="col-sm-2 control-label mandatory">Search for</label>
+			<div class="col-sm-10">
+				<input class="form-control" name="old" type="text" size="25" value="" />
+			</div>
+		</div><!-- .form-group -->
+		<div class="form-group row">
+			<label for="new" class="col-sm-2 control-label">Replace with</label>
+			<div class="col-sm-10">
+				<div class="input-group">
+					<div class="input-group-prepend btn btn-warning">
+						<input type="checkbox" name="replace" value="1" class="mt-1">
+					</div>
+					<input class="form-control" name="new" type="text" size="25" value="" />
+				</div>
+			</div>
+		</div><!-- .form-group -->
+		<div class="form-row">
+			<div class="controls save">
+				<button type="submit" name="btn" class="btn btn-primary" value="BTN_EXECUTE">%s</button> 
+				<button type="submit" name="btn" class="btn btn-secondary" value="BTN_CANCEL">%s</button>
+			</div><!-- .controls.save -->
+		</div>
+	'''%( btn_text_exec, btn_text_cncl))
+
+# ---------------------------------
+
+html.append('</div><!-- .card-body -->')
+html.append('</form><!-- .form-horizontal -->')
+html.append('</div><!-- #zmi-tab -->')
+html.append(context.zmi_body_footer(context,request))
+html.append('</body>')
+html.append('</html>')
+
+return '\n'.join(list(html))

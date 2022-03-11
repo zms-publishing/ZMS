86f0d0bc98d3c144ff52f4776d83a0e752fe199c
drfho <dr.frank.hoffmann@web.de>
2022-03-11 23:46:35 +0100

    refactored search/replace action (3)

--- Products/zms/conf/metacmd_manager/manage_searchReplace/manage_searchReplace.py
+++ Products/zms/conf/metacmd_manager/manage_searchReplace/manage_searchReplace.py
@@ -10,6 +10,8 @@
 from Products.zms import standard
 request = container.REQUEST
 RESPONSE = request.RESPONSE
+btn_text_exec = standard.pystr(context.getZMILangStr('BTN_EXECUTE'), encoding='utf-8', errors='replace')
+btn_text_cncl = standard.pystr(context.getZMILangStr('BTN_CANCEL'), encoding='utf-8', errors='replace')
 html=[]
 
 def replace(o, old, new):
@@ -53,11 +55,40 @@ html.append('<body class="%s">'%(' '.join(['zmi',request['lang'],'transition',co
 html.append(context.zmi_body_header(context,request,options=[{'action':'#','label':'Search+Replace...'}]))
 html.append('<div id="zmi-tab">')
 html.append(context.zmi_breadcrumbs(context,request))
+
 html.append('<form class="form-horizontal card" method="post" enctype="multipart/form-data">')
-html.append('<input type="hidden" name="form_id" value="manage_addClient"/>')
+html.append('<input type="hidden" name="form_id" value="manage_searchReplace"/>')
 html.append('<input type="hidden" name="lang" value="%s"/>'%request['lang'])
 html.append('<legend>Search+Replace...</legend>')
 html.append('<div class="card-body">')
+
+# --- Display initial insert form.
+# ---------------------------------
+html.append('''
+	<div class="form-group row">
+		<label for="old" class="col-sm-2 control-label mandatory">Search for</label>
+		<div class="col-sm-10">
+			<input class="form-control" name="old" type="text" size="25" value="" />
+		</div>
+	</div><!-- .form-group -->
+	<div class="form-group row">
+		<label for="new" class="col-sm-2 control-label">Replace with</label>
+		<div class="col-sm-10">
+			<div class="input-group">
+				<div class="input-group-prepend btn btn-warning">
+					<input type="checkbox" name="replace" value="1" class="mt-1">
+				</div>
+				<input class="form-control" name="new" type="text" size="25" value="" />
+			</div>
+		</div>
+	</div><!-- .form-group -->
+	<div class="form-row">
+		<div class="controls save">
+			<button type="submit" name="btn" class="btn btn-primary" value="BTN_EXECUTE">%s</button> 
+			<button type="submit" name="btn" class="btn btn-secondary" value="BTN_CANCEL">%s</button>
+		</div><!-- .controls.save -->
+	</div>
+'''%( btn_text_exec, btn_text_cncl))
 
 # --- Execute.
 # ---------------------------------
@@ -73,40 +104,17 @@ if request.form.get('btn')=='BTN_EXECUTE':
 	message.append('<ol>')
 	message.extend(['<li><a href="%s/manage_main" target="_blank">%s</a></li>'%(x.absolute_url(),x.absolute_url()) for x in res])
 	message.append('</ol>')
-	request.response.redirect(context.url_append_params('manage_main',{'lang':request['lang'],'manage_tabs_message':'\n'.join(message)}))
 
-# --- Display initial insert form.
-# ---------------------------------
-else:
-	btn_text_exec = standard.pystr(context.getZMILangStr('BTN_EXECUTE'), encoding='utf-8', errors='replace')
-	btn_text_cncl = standard.pystr(context.getZMILangStr('BTN_CANCEL'), encoding='utf-8', errors='replace')
 	html.append('''
-		<div class="form-group row">
-			<label for="old" class="col-sm-2 control-label mandatory">Search for</label>
-			<div class="col-sm-10">
-				<input class="form-control" name="old" type="text" size="25" value="" />
-			</div>
-		</div><!-- .form-group -->
-		<div class="form-group row">
-			<label for="new" class="col-sm-2 control-label">Replace with</label>
-			<div class="col-sm-10">
-				<div class="input-group">
-					<div class="input-group-prepend btn btn-warning">
-						<input type="checkbox" name="replace" value="1" class="mt-1">
-					</div>
-					<input class="form-control" name="new" type="text" size="25" value="" />
-				</div>
-			</div>
-		</div><!-- .form-group -->
-		<div class="form-row">
-			<div class="controls save">
-				<button type="submit" name="btn" class="btn btn-primary" value="BTN_EXECUTE">%s</button> 
-				<button type="submit" name="btn" class="btn btn-secondary" value="BTN_CANCEL">%s</button>
-			</div><!-- .controls.save -->
+		<div class="alert alert-success my-3">
+			<a class="close" style="font-size:1rem" data-dismiss="alert" href="#"><i class="fas fa-times"></i></a>
+			%s
 		</div>
-	'''%( btn_text_exec, btn_text_cncl))
+	'''%('\n'.join(message)))
 
-# ---------------------------------
+elif request.form.get('btn')=='BTN_CANCEL':
+	request.response.redirect(context.url_append_params('manage_main',{'lang':request['lang']}))
+
 
 html.append('</div><!-- .card-body -->')
 html.append('</form><!-- .form-horizontal -->')

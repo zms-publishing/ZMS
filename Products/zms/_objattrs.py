5299c0d11dbe7abd466bc2ff0128dc41737b77b2
zmsdev <zmsdev@sntl-publishing.com>
2022-03-29 21:00:10 +0200

    fixed local activity in primary lang

--- Products/zms/_objattrs.py
+++ Products/zms/_objattrs.py
@@ -45,7 +45,7 @@ def getobjattrdefault(obj, obj_attr, lang):
     if datatype in range(len(_globals.datatype_map)):
       default = obj_attr.get('default',_globals.datatype_map[datatype][1])
     # Default inactive in untranslated languages.
-    if obj_attr['id'] == 'active' and len(obj.getLangIds()) > 1 and not ( obj.isTranslated(lang,obj.REQUEST) or lang == obj.getPrimaryLanguage() ):
+    if obj_attr['id'] == 'active' and len(obj.getLangIds()) > 1 and not obj.isTranslated(lang,obj.REQUEST):
         default = 0
     if default is not None:
       if datatype in _globals.DT_DATETIMES and default == '{now}':
@@ -59,6 +59,8 @@ def getobjattrdefault(obj, obj_attr, lang):
         if default and datatype not in _globals.DT_TEXTS and isinstance(default, str):
           default = standard.dt_exec(obj,default)
         v = default
+    if obj.getSelf().id == 'e146':
+      standard.writeBlock(obj,str(('getobjattrdefault',obj_attr['id'],v,lang)))
     return v
 
 

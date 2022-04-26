1ef619803166f9c8659dc08e448e5c24a1252059
drfho <dr.frank.hoffmann@web.de>
2022-04-26 21:59:42 +0200

    avoid css  transform on  preview hover edit mark

--- Products/zms/plugins/www/zmi.core.js
+++ Products/zms/plugins/www/zmi.core.js
@@ -69,6 +69,8 @@ $ZMI.registerReady(function(){
 					cursor:pointer;
 					background:unset !important;
 					box-shadow: 0 0 0 1px black;
+				}
+				body:not(.zmi) .contentEditable.zmi-highlight:hover > * {
 					transform:unset !important;
 				}
 				body:not(.zmi) .contentEditable.zmi-highlight:hover:before {

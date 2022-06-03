a3d0c548cde6d7a97cb6c692256d3c264bcbcdb4
zmsbot <zmsbot@zms.hosting>
2022-06-04 00:12:06 +0200

    alternative to del ob._owner

--- Products/zms/zopeutil.py
+++ Products/zms/zopeutil.py
@@ -234,10 +234,12 @@ def initPermissions(container, id, permissions={}):
     manager_permissions = [x['name'] for x in ob.permissionsOfRole('Manager')]
     acquired_permissions = [x for x in manager_permissions if x not in role_permissions]
     ob.manage_acquiredPermissions(acquired_permissions)
-  # remove hidden owner to prevent auth-error
+  # modify hidden owner to prevent auth-error
   if hasattr(ob, '_owner'):
     # del ob._owner
-    ob._owner = tuple([],None)
+    ### as alternative: reduce attr's userfolder list ### 
+    owner_tuple = getattr(ob, '_owner', None)
+    ob._owner = tuple(([owner_tuple[0][-1]],owner_tuple[1]))
 
 def addDTMLMethod(container, id, title, data):
   """

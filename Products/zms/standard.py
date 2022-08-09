28e5fd46670fe86d4b13a947ccb36187db7323d9
Christian Meier <christian.meier@unibe.ch>
2022-08-09 18:08:54 +0200

    System-Info: Revised package display (#82)
    * System-Info: Revised package display
    
    Added "pip list" (shows Package version AND editable Project location if available) as supplementary info for "pip freeze -all" (shows Package version OR editable Git revision if available).
    
    CAUTION: The displayed version of Zope or other packages may be misleading if it was previously installed as an editable package.
    
    The version of an editable package persists for Zope (see also Control Panel), even if another Zope version has been post-installed via pip (see site-packages) -- but in fact the editable version is still used by the app server (even if pip uninstall was executed before). The version in site-packages can only be reactivated if the editable package folder is removed -- renaming and cleaning up .pth and .egg-info is not enough.
    
    To reflect version changes of an editable package (in pip list and Control Panel) it has to bee reinstalled via e.g. "pip install -e ../Zope -c ../Zope/constraints.txt" -- changes of Git revision (in pip freeze) are shown immediately.
    
    * added pip cmd alternating
    
    Co-authored-by: zmsbot <zmsbot@zms.hosting>

--- Products/zms/standard.py
+++ Products/zms/standard.py
@@ -253,12 +253,19 @@ def set_response_headers_cache(context, request=None, cache_max_age=24*3600, cac
 
 
 security.declarePublic('get_installed_packages')
-def get_installed_packages():
+def get_installed_packages(pip_cmd='freeze'):
   import subprocess
-  pipfreeze = subprocess.Popen("../../../bin/pip freeze --all",
-                               stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                               shell=True, cwd=getPACKAGE_HOME(), universal_newlines=True)
-  packages = pipfreeze.communicate()[0].strip()
+  pip_cmds = {
+      'list':'/pip list', 
+      'freeze':'/pip freeze --all'
+    }
+  cmd = pip_cmds.get(pip_cmd,'freeze')
+  pth = getPACKAGE_HOME().rsplit('/lib/')[0] + '/bin'
+  packages = ''
+  output = subprocess.Popen(pth + cmd,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                            shell=True, cwd=pth, universal_newlines=True)
+  packages = f'# {pth}{cmd}\n\n{output.communicate()[0].strip()}'
   return packages
 
 

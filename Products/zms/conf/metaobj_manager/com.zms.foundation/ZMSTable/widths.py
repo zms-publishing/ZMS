b28f09c445456e6fb23f2f134d95b0810a84168f
drfho <dr.frank.hoffmann@web.de>
2022-09-15 18:01:39 +0200

    ZMSTable: set caption field as mandatory (#89)
    * Normalized zmi template syntax
    * set caption input to mandatory, cleaning

--- Products/zms/conf/metaobj_manager/com.zms.foundation/ZMSTable/widths.py
+++ Products/zms/conf/metaobj_manager/com.zms.foundation/ZMSTable/widths.py
@@ -8,17 +8,21 @@
 ##title=py: Col-Widths [list]
 ##
 # --// BO widths //--
-
+from Products.zms import standard
 REQUEST = zmscontext.REQUEST
 table = zmscontext.attr('table')
-ncols = max([len(x) for x in table])
-weights = [1 for x in range( ncols)]
-for row in table:
-    i = 0
-    for cell in row:
-        weight = len(zmscontext.re_sub('<(.*?)>', '',zmscontext.dt_exec(cell.get('content',''))))
-        weights[i] = weights[i] + weight
-        i += 1
-return [int((x*100.0)/sum(weights)) for x in weights]
+try:
+	ncols = max([len(x) for x in table])
+	weights = [1 for x in range( ncols)]
+	for row in table:
+		i = 0
+		for cell in row:
+			weight = len(zmscontext.re_sub('<(.*?)>', '',zmscontext.dt_exec(cell.get('content',''))))
+			weights[i] = weights[i] + weight
+			i += 1
+	return list([int((x*100.0)/sum(weights)) for x in weights])
+except:
+	standard.writeError(zmscontext, 'ZMSTable.widths: weights not computable %s'%(table))
+	return list([])
 
 # --// EO widths //--

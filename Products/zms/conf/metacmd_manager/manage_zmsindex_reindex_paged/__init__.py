b7d2e0a51d3ebcfd8e4f48379641db0ba9c6bdfc
zmsdev <zmsdeveloper@gmail.com>
2023-06-27 09:53:47 +0200

    ZMSIndex: Reindex paged

---
+++ Products/zms/conf/metacmd_manager/manage_zmsindex_reindex_paged/__init__.py
@@ -0,0 +1,51 @@
+class manage_zmsindex_reindex_paged:
+	"""
+	python-representation of manage_zmsindex_reindex_paged
+	"""
+
+	# Acquired
+	acquired = 0
+
+	# Action
+	action = "%smanage_executeMetacmd?id=manage_zmsindex_reindex_paged"
+
+	# Description
+	description = ""
+
+	# Execution
+	execution = False
+
+	# Icon_clazz
+	icon_clazz = "fas fa-cogs"
+
+	# Id
+	id = "manage_zmsindex_reindex_paged"
+
+	# Meta_types
+	meta_types = ["ZMS"]
+
+	# Name
+	name = "ZMSIndex: Reindex paged"
+
+	# Nodes
+	nodes = "{$}"
+
+	# Package
+	package = "com.zms.foundation.zmsindex"
+
+	# Revision
+	revision = "0.0.2"
+
+	# Roles
+	roles = ["ZMSAdministrator"]
+
+	# Stereotype
+	stereotype = ""
+
+	# Title
+	title = "ZMSIndex: Reindex paged"
+
+	# Impl
+	class Impl:
+		manage_zmsindex_reindex_paged = {"id":"manage_zmsindex_reindex_paged"
+			,"type":"External Method"}

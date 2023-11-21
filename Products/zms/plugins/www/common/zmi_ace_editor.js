dda4cbaa7eb5283fe09e670cb69ca903588379d5
drfho <dr.frank.hoffmann@web.de>
2023-11-21 04:16:36 +0100

    zmi: added error info attr method

--- Products/zms/plugins/www/common/zmi_ace_editor.js
+++ Products/zms/plugins/www/common/zmi_ace_editor.js
@@ -20,7 +20,12 @@ function show_ace_editor(e, toggle=false) {
 		$textarea.addClass('form-control-ace-editor');
 		$textarea.parent().addClass('zmi-ace-editor')
 		$textarea.after('<div id="' + editor_id + '">ace editor text</div>');
-		$textarea.closest('td').append('<div class="control-label-ace-editor"><div><i onclick="remove_ace_editor(\'' + editor_id + '\')" title="Close Editor" class="close fas fa-chevron-up text-primary"></i></div><div><a title="Zope-Object-Editor" target="_blank" href="' + zopeurl + '">' + $textarea.attr('id').split('attr_custom_')[1] + '</a></div></div>');
+		if (zopeurl == '#/manage') {
+			// Zope-Object is not yet created: Products.zms.zopeutil.MissingArtefactProxy
+			$textarea.closest('td').append('<div class="control-label-ace-editor"><div><i onclick="remove_ace_editor(\'' + editor_id + '\')" title="Close Editor" class="close fas fa-chevron-up text-primary"></i></div></div>');
+		} else {
+			$textarea.closest('td').append('<div class="control-label-ace-editor"><div><i onclick="remove_ace_editor(\'' + editor_id + '\')" title="Close Editor" class="close fas fa-chevron-up text-primary"></i></div><div><a title="Zope-Object-Editor" target="_blank" href="' + zopeurl + '">' + $textarea.attr('id').split('attr_custom_')[1] + '</a></div></div>');
+		}
 	}
 
 	var dom = ace.require('ace/lib/dom');

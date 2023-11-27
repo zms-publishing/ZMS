177dc497bc5603ae39dddf393bdb5017f1ac98f3
drfho <dr.frank.hoffmann@web.de>
2023-11-27 06:13:15 +0100

    opensearch_init: use opensearchpy

--- Products/zms/conf/metaobj_manager/com.zms.foundation.catalog.opensearch/opensearch_interface/manage_opensearch_init.py
+++ Products/zms/conf/metaobj_manager/com.zms.foundation.catalog.opensearch/opensearch_interface/manage_opensearch_init.py
@@ -1,36 +1,54 @@
 from Products.zms import standard
 import json
-import requests
-from requests.auth import HTTPBasicAuth
-requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+from urllib.parse import urlparse
+import opensearchpy
+from opensearchpy import OpenSearch
+
 # import pdb
+# pdb.set_trace()
 
 def manage_opensearch_init( self):
 
-  resp_text = '//RESPONSE\n'
-  url = self.getConfProperty('opensearch.url')
-  username = self.getConfProperty('opensearch.username', 'admin')
-  password = self.getConfProperty('opensearch.password', 'admin')
-  schema = self.getConfProperty('opensearch.schema','{}')
-  verify = bool(self.getConfProperty('opensearch.ssl.verify', ''))
-  auth = HTTPBasicAuth(username,password)
-  headers = {'Content-type': 'application/x-ndjson'}
-  # pdb.set_trace()
-  root_id = self.getRootElement().getHome().id
+	index_name = self.getRootElement().getHome().id
+	resp_text = '//RESPONSE\n'
 
-  try:
-    resp_text += 'PUT %s\n'%('%s/%s'%(url,root_id))
-    response = requests.put('%s/%s'%(url,root_id),auth=auth,headers=headers,json=schema,verify=verify)
-    resp_text += json.dumps(response.json(), separators=(",", ":"), indent=2)
-    response.raise_for_status()
-  except:
-    resp_text += '\n\n'
-    resp_text += '//ERROR\n'
-    resp_text += standard.writeError(self,'Error: %s'%(resp_text))
-  
-  # concatinate opensearch response and schema
-  resp_text += '\n\n'
-  resp_text += '//SCHEMA\n'
-  resp_text += schema
+	url = self.getConfProperty('opensearch.url')
+	host = urlparse(url).hostname
+	port = urlparse(url).port
+	ssl = urlparse(url).scheme=='https' and True or False
+	verify = bool(self.getConfProperty('opensearch.ssl.verify', False))
 
-  return resp_text
+	username = self.getConfProperty('opensearch.username', 'admin')
+	password = self.getConfProperty('opensearch.password', 'admin')
+	auth = (username,password)
+
+	schema = self.getConfProperty('opensearch.schema','{}')
+
+
+	client = OpenSearch(
+		hosts = [{'host': host, 'port': port}],
+		http_compress = False, # enables gzip compression for request bodies
+		http_auth = auth,
+		use_ssl = ssl,
+		verify_certs = verify,
+		ssl_assert_hostname = False,
+		ssl_show_warn = False,
+	)
+
+	try:
+		response = client.indices.create(index_name, body=schema)
+	except opensearchpy.exceptions.RequestError as e:
+		if 'resource_already_exists_exception' != e.error:
+			raise
+		else:
+			client.indices.delete(index_name)
+			response = client.indices.create(index_name, body=schema)
+			resp_text += '//%s\n'%(e.error)
+	resp_text += json.dumps(response, indent=2)
+
+	# concatinate opensearch response and schema
+	resp_text += '\n\n'
+	resp_text += '//SCHEMA\n'
+	resp_text += schema
+
+	return resp_text

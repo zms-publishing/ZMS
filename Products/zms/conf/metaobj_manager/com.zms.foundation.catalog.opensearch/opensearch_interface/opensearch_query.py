94053904b77207fc034aed05d87855aa926c95ed
drfho <dr.frank.hoffmann@web.de>
2023-11-28 15:29:22 +0100

    using opensearch-py for opensearch_query
    Ref: https://github.com/zms-publishing/ZMS/commit/0931acdf7c4960a0e898582fb328e827baca8c97

--- Products/zms/conf/metaobj_manager/com.zms.foundation.catalog.opensearch/opensearch_interface/opensearch_query.py
+++ Products/zms/conf/metaobj_manager/com.zms.foundation.catalog.opensearch/opensearch_interface/opensearch_query.py
@@ -1,47 +1,79 @@
-import requests
-from requests.auth import HTTPBasicAuth
+from Products.zms import standard
 import json
+from urllib.parse import urlparse
+import opensearchpy
+from opensearchpy import OpenSearch
+from opensearchpy.helpers import bulk
+
+
+def get_opensearch_client(self):
+	# ${opensearch.url:https://localhost:9200}
+	# ${opensearch.username:admin}
+	# ${opensearch.password:admin}
+	# ${opensearch.ssl.verify:}
+	url = self.getConfProperty('opensearch.url')
+	if not url:
+		return None
+	host = urlparse(url).hostname
+	port = urlparse(url).port
+	ssl = urlparse(url).scheme=='https' and True or False
+	verify = bool(self.getConfProperty('opensearch.ssl.verify', False))
+	username = self.getConfProperty('opensearch.username', 'admin')
+	password = self.getConfProperty('opensearch.password', 'admin')
+	auth = (username,password)
+	
+	client = OpenSearch(
+		hosts = [{'host': host, 'port': port}],
+		http_compress = False, # enables gzip compression for request bodies
+		http_auth = auth,
+		use_ssl = ssl,
+		verify_certs = verify,
+		ssl_assert_hostname = False,
+		ssl_show_warn = False,
+	)
+	return client
+
 
 def opensearch_query( self, q):
-  request = self.REQUEST
-  qpage_index = request.get('pageIndex',0)
-  qsize = request.get('size', 10)
-  qfrom = request.get('from', qpage_index*qsize)
+	REQUEST = self.REQUEST
+	qpage_index = REQUEST.get('pageIndex',0)
+	qsize = REQUEST.get('size', 10)
+	qfrom = REQUEST.get('from', qpage_index*qsize)
+	index_name = self.getRootElement().getHome().id
+	resp_text = ''
 
-  d = {
-    "size": qsize,
-    "from": qfrom,
-    "query":{
-      "query_string":{"query":q}
-    },
-    "highlight": {
-      "fields": {
-        "title": { "type": "plain"},
-        "standard_html": { "type": "plain"}
-      }
-    },
-    "aggs": {
-      "response_codes": {
-        "terms": {
-          "field": "meta_id",
-          "size": 5
-        }
-      }
-    }
-  }
+	query = {
+		"size": qsize,
+		"from":qfrom,
+		"query":{
+			"query_string":{"query":q}
+		},
+		"highlight": {
+			"fields": {
+				"title": { "type": "plain"},
+				"standard_html": { "type": "plain"}
+			}
+		},
+		"aggs": {
+			"response_codes": {
+				"terms": {
+					"field": "meta_id",
+					"size": 5
+				}
+			}
+		}
+	}
 
-  url = self.getConfProperty('opensearch.url', 'https://localhost:9200')
-  # ID of opensearch index is ZMS multisite root node id or explicitly given by request variable 'opensearch_index_id'
-  root_id = self.getRootElement().getHome().id
-  index_id = request.get('opensearch_index_id',root_id)
-  username = self.getConfProperty('opensearch.username', 'admin')
-  password = self.getConfProperty('opensearch.password', 'admin')
-  verify = bool(self.getConfProperty('opensearch.ssl.verify', False))
-  auth = HTTPBasicAuth(username,password)
-  response = requests.get('%s/%s/_search?pretty=true'%(url,index_id),auth=auth,json=d,verify=verify)
-  response.raise_for_status()
-  json_obj = response.json()
-  data = json.dumps(json_obj, separators=(",", ":"), indent=4)
-  request.RESPONSE.setHeader('Content-Type','text/json; charset=utf-8')
-  return data
+	client = get_opensearch_client(self)
+	if not client:
+		return '{"error":"No client"}'
+
+	try:
+		response = client.search(body = json.dumps(query), index = index_name)
+		resp_text = json.dumps(response)
+	except opensearchpy.exceptions.RequestError as e:
+		resp_text = '//%s'%(e.error)
+
+	REQUEST.RESPONSE.setHeader('Content-Type','application/json; charset=utf-8')
+	return resp_text
 

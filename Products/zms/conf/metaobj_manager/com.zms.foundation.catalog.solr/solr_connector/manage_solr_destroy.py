356412458eb4913009faf2425e5bea279924c81e
drfho <dr.frank.hoffmann@web.de>
2023-12-05 18:38:05 +0100

    fixed print

--- Products/zms/conf/metaobj_manager/com.zms.foundation.catalog.solr/solr_connector/manage_solr_destroy.py
+++ Products/zms/conf/metaobj_manager/com.zms.foundation.catalog.solr/solr_connector/manage_solr_destroy.py
@@ -1,6 +1,7 @@
 from Products.zms import standard
 import json
 from urllib.parse import urlparse
+import requests
 
 def manage_solr_destroy( self):
 
@@ -9,16 +10,16 @@ def manage_solr_destroy( self):
 	# ${solr.password:admin}
 	# ${solr.ssl.verify:}
 	url = self.getConfProperty('solr.url')
+	resp = {}
 	if not url:
 		return None
 	username = self.getConfProperty('solr.username', 'admin')
 	password = self.getConfProperty('solr.password', 'admin')
 	auth = (username,password)
 
-	import requests
 	index_name = self.getRootElement().getHome().id
 	response = requests.post('%s/%s/update'%(url,index_name), auth=auth, json={'delete':{"query":"*"}, 'commit':{}})
-	print(f"Status Code: {response.status_code}, Response: {response.json()}")
+	resp['Status Code'] = response.status_code
+	resp['Solr Response'] = response.json()
 
-	resp_text += json.dumps(response, indent=2)
-	return resp_text
+	return json.dumps(resp, indent=2)

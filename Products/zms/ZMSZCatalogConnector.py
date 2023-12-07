e319512e67d9a05605efdf05ee30dc662898336a
zmsdev <zmsdev@sntl-publishing.com>
2023-12-07 18:23:23 +0100

    restored search_xml

--- Products/zms/ZMSZCatalogConnector.py
+++ Products/zms/ZMSZCatalogConnector.py
@@ -21,6 +21,7 @@ from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from zope.interface import implementer
 import json
 import re
+import sys
 # Product Imports.
 from Products.zms import standard
 from Products.zms import IZMSCatalogConnector
@@ -168,8 +169,78 @@ class ZMSZCatalogConnector(
       """ search_json """
       RESPONSE.setHeader('Cache-Control', 'no-cache')
       RESPONSE.setHeader('Content-Type', 'application/json; charset=utf-8')
-      result = [x['ob'](self, REQUEST) for x in self.getActions(r'(.*?)_query$')][0]
-      return result
+      results = [x['ob'](self, REQUEST) for x in self.getActions(r'(.*?)_query$')][0]
+      return results
+
+    # --------------------------------------------------------------------------
+    #  ZMSZCatalogConnector.search_xml
+    # --------------------------------------------------------------------------
+    def search_xml(self, REQUEST, RESPONSE):
+      """ search_xml """
+      RESPONSE.setHeader('Cache-Control', 'no-cache')
+      RESPONSE.setHeader('Content-Type', 'text/xml; charset=utf-8')
+      results = json.load([x['ob'](self, REQUEST) for x in self.getActions(r'(.*?)_query$')][0])
+      # Assemble xml.
+      status = 0
+      page_index = int(REQUEST.get('page_index',0))
+      page_size = int(REQUEST.get('page_size',20))
+      xml = self.getXmlHeader()
+      xml += '<response>'
+      xml += '<lst name="responseHeader">'
+      xml += '<int name="status">%i</int>'%status
+      xml += '<lst name="params">'
+      for key in REQUEST.form.keys():
+        xml += '<str name="%s">%s</str>'%(key, standard.html_quote(REQUEST.form[key]))
+      xml += '</lst>'
+      xml += '</lst>'
+      xmlr = ''
+      if status <= 0:
+        xmlr += '<result name="response" numFound="%i" start="%i">'%(len(results), page_index*page_size)
+        if len(results) > page_size:
+          results = results[page_index*page_size:(page_index+1)*page_size]
+        for result in results:
+          xmlr += '<doc>'
+          for k in result.keys():
+            try:
+              v = result[k]
+              if k == 'zcat_column_loc':
+                k = 'loc'
+              elif k == 'zcat_column_index_html':
+                k = 'index_html'
+              elif k == 'zcat_column_custom':
+                k = 'custom'
+              elif k == 'standard_html':
+                v = standard.remove_tags(v)
+              xmlr += '<arr name="%s">'%k
+              if isinstance(v,str):
+                for x in range(16):
+                  v = v.replace(chr(x), '')
+              if k == 'custom':
+                xmlr += '<str>%s</str>'%v
+              else:
+                xmlr += '<str><![CDATA[%s]]></str>'%v
+              xmlr += '</arr>'
+            except:
+              standard.writeError(self, '[search_xml]: result=%s, k=%s'%(str(result), k))
+              t, v, tb = sys.exc_info()
+              status = 400
+              msg = v
+              break
+          xmlr += '</doc>'
+        xmlr += '</result>'
+      if status > 0:
+        xmlr = ''
+        xmlr += '<lst name="error">'
+        xmlr += '<str name="msg">%s</str>'%standard.html_quote(msg)
+        xmlr += '<int name="code">%i</int>'%status
+        xmlr += '</lst>'
+      xml += str(xmlr)
+      xml += '</response>'
+      if pretty!=0:
+        # Prettify xml
+        xml = minidom.parseString(xml).toprettyxml(indent='  ')
+      return xml
+
 
     # --------------------------------------------------------------------------
     #  ZMSZCatalogConnector.reindex_page

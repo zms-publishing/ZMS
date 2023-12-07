934819ea01b53ae5fdfcc65b4cd064be0d4aa8ca
drfho <dr.frank.hoffmann@web.de>
2023-12-07 17:25:58 +0100

    fix test

--- tests/test_rest_api.py
+++ tests/test_rest_api.py
@@ -54,7 +54,7 @@ class RestAPITest(ZMSTestCase):
       actual = json.loads( self.context.__bobo_traverse__(request, name)(request))
       print(json.dumps(actual))
       self.assertTrue(isinstance(actual, dict))
-      self.assertEqual( len(actual), 47)
+      self.assertEqual( len(actual), 39)
 
   def test_headless_get(self):
       count = 0

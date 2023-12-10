3bf060c231662a5b64e9034f8ceac1d0c635407a
zmsbot <zmsbot@zms.hosting>
2023-12-10 13:06:58 +0100

    fix zcatalog_suggest

--- Products/zms/conf/metaobj_manager/com.zms.catalog.zcatalog/zcatalog_connector/zcatalog_suggest.py
+++ Products/zms/conf/metaobj_manager/com.zms.catalog.zcatalog/zcatalog_connector/zcatalog_suggest.py
@@ -1,5 +1,8 @@
+from Products.zms import standard
+
 def zcatalog_suggest(self, REQUEST=None):
   request = self.REQUEST
+  q = request.get('q','')
   limit = int(REQUEST.get('limit',5))
   lang = standard.nvl(request.get('lang'), self.getPrimaryLanguage())
   root = self.getRootElement()
@@ -8,11 +11,14 @@ def zcatalog_suggest(self, REQUEST=None):
   # Find suggest-results.
   rtn = []
   lexicon = zcatalog.Lexicon
-  for w in lexicon.words():
-    if w[0] not in ['_', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] and w.lower().find(q) >= 0 and w not in rtn:
+  ### All word
+  # rtn = [x for x in list(lexicon.words()) if x.lower().find(q.lower())>=0]
+  ### Filter/limit words
+  for w in list(lexicon.words()):
+    if (w[0] not in ['_', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']) and (w.lower().find(q.lower()) >= 0) and (w not in rtn):
       rtn.append(w)
     if len(rtn) >= limit:
       break
-    
+
   # Return list of suggestions.
   return rtn

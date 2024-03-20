adab2c2d61ef28dc586d61ae9de48ee510df6240
drfho <dr.frank.hoffmann@web.de>
2024-03-20 15:18:52 +0100

    fix: use global connector if local not available

--- Products/zms/ZMSZCatalogAdapter.py
+++ Products/zms/ZMSZCatalogAdapter.py
@@ -157,8 +157,16 @@ class ZMSZCatalogAdapter(ZMSItem.ZMSItem):
           for node in nodes:
             if self.matches_ids_filter(node):
               fileparsing = standard.pybool( self.getConfProperty('ZMS.CatalogAwareness.fileparsing', 1))
-              for connector in self.get_connectors():
-                self.reindex(connector, node, recursive=False, fileparsing=fileparsing)
+              if self.get_connectors():
+                # if local connectors are available, use them
+                # Note: if local connectors are used, they must cover all connector types
+                for connector in self.get_connectors():
+                  self.reindex(connector, node, recursive=False, fileparsing=fileparsing)
+              else:
+                # otherwise use global connector
+                root = self.getRootElement()
+                for connector in root.getCatalogAdapter().get_connectors():
+                  self.reindex(connector, node, recursive=False, fileparsing=fileparsing)
         return True
       except:
         standard.writeError( self, "can't reindex_node")

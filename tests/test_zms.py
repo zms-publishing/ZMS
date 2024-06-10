7f186f312737a999e8eba64e3f5b95bb9ec04f77
zmsdev <zmsdeveloper@gmail.com>
2024-06-10 18:16:33 +0200

    super // test_zcatalog_adapter

--- tests/test_zms.py
+++ tests/test_zms.py
@@ -4,47 +4,40 @@ from OFS.Folder import Folder
 
 # Product imports.
 from tests.zms_test_util import *
-from Products.zms import mock_http
-from Products.zms import zms
 
 # /Products/zms> python -m unittest discover -s unit_tests
 # /Products/zms> python -m unittest test_zms.ZMSTest
-class ZMSTest(ZMSTestCase):
+class ZMSTest(ZMSPortalTestCase):
 
   temp_title = 'temp-test'
 
   def setUp(self):
-    folder = Folder('myzmsx')
-    folder.REQUEST = mock_http.MockHTTPRequest({'lang':'eng','preview':'preview'})
-    zmscontext = zms.initZMS(folder, 'content', 'titlealt', 'title', 'eng', 'eng', folder.REQUEST)
-    self.context = zmscontext
+    super(ZMSTest, self).setUp()
     print('[setUp] create %s'%self.temp_title)
-    self.folder = zmscontext.manage_addZMSCustom('ZMSFolder',{'title':self.temp_title,'titlealt':self.temp_title},zmscontext.REQUEST)
+    zmscontext = self.context
+    clients = zmscontext.getPortalClients()
+    clients[0].manage_delObjects([clients[0].getCatalogAdapter().id])
+
 
   def test_portal(self):
-    context = self.context
-    request = context.REQUEST
-    
-    home = context.aq_parent
-    ids = []
-    clients = []
-    n = 3
-    for i in range(n):
-      id = 'client%i'%i
-      client = Folder(id)
-      setattr(client, home.id, home)
-      home._setObject(client.id, client)
-      client = getattr(home, client.id)
-      zmscontext = zms.initZMS(client, 'content', id, id, 'eng', 'eng', request)
-      zmscontext.setConfProperty('Portal.Master',home.id)
-      ids.append(id)
-      clients.append(zmscontext)
-    context.setConfProperty('Portal.Clients',ids)
-    self.assertEqual(n,len(context.getPortalClients()))
+    zmscontext = self.context
+    clients = zmscontext.getPortalClients()
+    self.assertEqual(3, len(clients))
     for client in clients:
       self.assertIsNotNone(client.getPortalMaster())
-    
-  def tearDown(self):
+  
+  def test_zcatalog_adapter(self):
     zmscontext = self.context
+    catalog_adapter = zmscontext.getCatalogAdapter()
+    self.assertIsNotNone(catalog_adapter)
+    # Clients have parent catalog-adapter.
+    clients = zmscontext.getPortalClients()
+    for i in range(len(clients)):
+      client = clients[i]
+      if i == 0:
+        self.assertEqual(catalog_adapter, client.getCatalogAdapter())
+      else:
+        self.assertNotEqual(catalog_adapter, client.getCatalogAdapter())
+
+  def tearDown(self):
     print('[tearDown] remove %s'%self.temp_title)
-    zmscontext.manage_delObjects([self.folder.id])

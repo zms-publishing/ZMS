8066995e93bb75f2e162a4f97e62259c8efc9a22
drfho <dr.frank.hoffmann@web.de>
2025-03-08 14:21:00 +0100

    added example JS for child-count in trashcan to default-theme

--- Products/zms/conf/metaobj_manager/com.zms.foundation.theme/zms_theme_basic/zmi.js
+++ Products/zms/conf/metaobj_manager/com.zms.foundation.theme/zms_theme_basic/zmi.js
@@ -1,1 +1,19 @@
-// EMPTY
+// CUSTOMISATION EXAMPLE:
+// This script uses the REST API to get the number of child nodes in the trashcan
+// and shows the trashcan icon in the header if the number is greater than 0
+$ZMI.registerReady(function() {
+	fetch($ZMI.getBaseUrl() + '/++rest_api/trashcan/get_child_nodes/count')
+	.then(
+		response => response.text())
+		.then(data => {
+			if (parseInt(data) > 0) {
+				$('header #toggle_menu').after(`
+					<li id="trashrestore" class="form-inline desktop hidden-xs" title="${data} Object(s) in Trashcan">
+						<a target="_top" href="${$ZMI.getBaseUrl()}/trashcan/manage?lang=ger">
+							<i class="fas fa-trash-restore"></i>
+						</a>
+					</li>`)
+			};
+		})
+	.catch(error => console.error('Error ++rest_api:', error));
+})

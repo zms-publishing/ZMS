8aa8fc3cbb484b9a81e19b2c1e8cdac22163d8a7
drfho <dr.frank.hoffmann@web.de>
2025-06-24 13:43:20 +0200

    added com.zms.demo lib

---
+++ Products/zms/conf/metaobj_manager/com.zms.demo/__init__.py
@@ -0,0 +1,22 @@
+class com_zms_demo:
+	"""
+	python-representation of com.zms.demo
+	"""
+
+	# Enabled
+	enabled = 1
+
+	# Id
+	id = "com.zms.demo"
+
+	# Name
+	name = "com.zms.demo"
+
+	# Package
+	package = ""
+
+	# Revision
+	revision = "5.0.0"
+
+	# Type
+	type = "ZMSPackage"
---
+++ Products/zms/conf/metaobj_manager/com.zms.demo/zmsdemo_helpers/__init__.py
@@ -0,0 +1,39 @@
+class zmsdemo_helpers:
+	"""
+	python-representation of zmsdemo_helpers
+	"""
+
+	# Access
+	access = {"delete_custom":""
+		,"delete_deny":[]
+		,"insert_custom":""
+		,"insert_deny":[]}
+
+	# Enabled
+	enabled = 0
+
+	# Id
+	id = "zmsdemo_helpers"
+
+	# Name
+	name = "zmsdemo_helpers"
+
+	# Package
+	package = "com.zms.demo"
+
+	# Revision
+	revision = "5.0.0"
+
+	# Type
+	type = "ZMSLibrary"
+
+	# Attrs
+	class Attrs:
+		manage_zmsdemo_process = {"default":""
+			,"id":"manage_zmsdemo_process"
+			,"keys":[]
+			,"mandatory":0
+			,"multilang":0
+			,"name":"Export and Refresh ZMS-Demo-Site"
+			,"repetitive":0
+			,"type":"External Method"}

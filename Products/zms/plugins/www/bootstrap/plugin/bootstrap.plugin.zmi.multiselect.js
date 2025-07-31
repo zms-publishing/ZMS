0f1ebe5413ff94dac3ea20d35d682420d049e7e5
drfho <dr.frank.hoffmann@web.de>
2025-08-01 00:10:51 +0200

    added filter to multiselect menu, refact metaobj filter
    Ref: https://github.com/idasm-unibe-ch/unibe-cms/issues/986
    Todo: "tablefilter" (derived from zope objectlist) shall be generally renamed to ZMI "menufilter"

--- Products/zms/plugins/www/bootstrap/plugin/bootstrap.plugin.zmi.multiselect.js
+++ Products/zms/plugins/www/bootstrap/plugin/bootstrap.plugin.zmi.multiselect.js
@@ -1,5 +1,8 @@
+// Hint: To be cached by the browser in sessionStorage
 ZMI.prototype.multiselect = function(context) {
+	let multiselect_index = 0;
 	$("select.zmi-select[multiple]:not(.d-none)",context).each(function() {
+		multiselect_index++;
 		var refreshContainer = function($container) {
 			var c = 0;
 			var $select = $container.parent().prev();
@@ -20,33 +23,51 @@ ZMI.prototype.multiselect = function(context) {
 			else {
 				$dropdown.show("normal");
 			}
-		}
+		};
 		var $select = $(this);
 		var $select_disabled = ( $select.attr('disabled')=='disabled' ) ? ' disabled="disabled" ': '';
 		$select.next(".zmi-select-container").remove();
-		var html = ''
-			+ '<div class="zmi-select-container form-inline">'
-			+ '<div class="'+$select.attr('class').replace('form-control','')+'">'
-			+ '</div>'
-			+ '<div class="btn-group btn-group-sortable mt-2">\n'
-			+ '<button type="button"' + $select_disabled + 'class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\n'
-			+ '<i class="fas fa-plus mr-1"></i>'
-			+ '</button>'
-			+ '<div class="dropdown-menu scrollable-menu">';
-		var c = 0;
+
+		var html = `
+			<div class="zmi-select-container form-inline">
+				<div class="${$select.attr('class').replace('form-control','')}"></div>
+				<div class="btn-group btn-group-sortable mt-2">
+					<button type="button" ${$select_disabled} 
+						class="btn btn-secondary dropdown-toggle" 
+						data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<i class="fas fa-plus mr-1"></i>
+					</button>
+					<div class="dropdown-menu scrollable-menu">`;
+
+		var option_count = $("option",$select).length;
+		if (option_count==0) {
+			html += '<a class="dropdown-item" href="javascript:;">(No Data)</a>';
+		} else if (option_count > 12) {
+			html += `
+				<div class="dropdown-header menufilter">
+					<span class="btn-group">
+						<label for="menufilter${multiselect_index}" 
+							onmouseover="$('#menufilter${multiselect_index}').focus()">
+							<i class="fas fa-filter menufilter" style="cursor:default;"></i>
+						</label>
+						<input id="menufilter${multiselect_index}"
+							placeholder="${getZMILangStr('CAPTION_CHOOSEOBJ')}"
+							name="menufilter" type="text" 
+							class="form-control dropdown-header"
+							title="Filter object list by entering name or id" />
+					</span>
+				</div>`;
+		};
 		$("option",$select).each(function() {
 			var v = $(this).attr('value');
 			$(this).attr('data-value',v);
-			html += ''
-				+ '<a class="dropdown-item" href="javascript:;" data-value="'+v+'">\n'
-				+ $(this).text()
-				+ '</a>\n';
-			c++;
+			html += `<a class="dropdown-item" href="javascript:;" data-value="${v}">${$(this).text()}</a>\n`;
 		});
-		html += ''
-			+ '</div>'
-			+ '</div>'
-			+ '</div>';
+		html += `
+					</div><!-- dropdown-menu -->
+				</div><!-- btn-group -->
+			</div><!-- zmi-select-container -->`;
+
 		$select.addClass("d-none").after(html);
 		var $container = $select.next().children(":first");
 		var $dropdown = $container.next();
@@ -55,18 +76,9 @@ ZMI.prototype.multiselect = function(context) {
 			$(this).addClass("d-none");
 			var data_value = $(this).attr('data-value');
 			if ( $select_disabled ) {
-				$container.append(''
-					+ '<div class="btn bg-light mt-2" disabled="disabled">'
-					+ $(this).text()
-					+ '</div> '
-				);
+				$container.append(`<div class="btn bg-light mt-2" disabled="disabled">${$(this).text()}</div>`);
 			} else {
-				$container.append(''
-					+ '<div class="btn btn-light mt-2" data-value="'+data_value+'">'
-					+ '<a href="javascript:;"><i class="fas fa-times"></i></a> '
-					+ $(this).text()
-					+ '</div> '
-				);
+				$container.append(`<div class="btn btn-light mt-2" data-value="${data_value}"><a href="javascript:;"><i class="fas fa-times"></i></a> ${$(this).text()}</div>`);
 			};
 			$(".btn a:last",$container).click(function() {
 				var $parent = $(this).parent();
@@ -91,5 +103,43 @@ ZMI.prototype.multiselect = function(context) {
 				refreshContainer($container);
 			});
 		});
+
+		// +++++++++++++++++++++++++++
+		// Menufilter: Object-List
+		// +++++++++++++++++++++++++++
+		$('#menufilter'+multiselect_index).keyup(function (event) {
+			let menufilter = $(this).val();
+			if (isModifierKeyPressed(event)) {
+				return; // ignore
+			}
+			if (event.which == 13) {
+				event.preventDefault();
+			};
+			$("a.dropdown-item",$dropdown).each(function() {
+				if ($(this).text().toLowerCase().indexOf(menufilter.toLowerCase()) >= 0) {
+					$(this).show();
+				} else {
+					$(this).hide();
+				}
+			});
+		});
+
+		// Reset filter on dropdown shown
+		$dropdown.parent().on('shown.bs.dropdown', function () {
+			$('#menufilter' + multiselect_index).val('').trigger('keyup').focus();
+		});
+
 	});
+
+	// Function to check if a modifier key is pressed
+	/**
+	 * Check if a modifier key (Alt, Ctrl, Meta) is pressed.
+	 * @param {Event} event - The keyboard event to check.
+	 * @returns {boolean} - True if a modifier key is pressed, false otherwise.
+	 */
+	function isModifierKeyPressed(event) {
+		return event.altKey ||
+		event.ctrlKey ||
+		event.metaKey;
+	}
 }

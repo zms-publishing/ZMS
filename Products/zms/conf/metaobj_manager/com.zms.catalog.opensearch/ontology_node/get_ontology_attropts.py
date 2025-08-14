1f9e12a100a9d6682ebd3f43e5f0277e7ad6bb14
drfho <dr.frank.hoffmann@web.de>
2025-08-15 00:50:52 +0200

    refact ontology proessing (at work)

--- Products/zms/conf/metaobj_manager/com.zms.catalog.opensearch/ontology_node/get_ontology_attropts.py
+++ Products/zms/conf/metaobj_manager/com.zms.catalog.opensearch/ontology_node/get_ontology_attropts.py
@@ -2,28 +2,13 @@
 
 def get_ontology_attropts(self):
 	request = self.REQUEST
-	lang = request.get('lang', self.getPrimaryLanguage())
-	langs = self.getLanguages(request)
-	# Get ontology records by acquisition along the breadcrumb path.
 	ontology = self.get_ontology()
-	# If function get_ontology returns a string as warning message.
-	if isinstance(ontology, str):
-		return [('', ontology)]
+
 	# Create a list of attribute options for each facet key.
 	attropts = []
-	for facet_key in list(ontology.keys()):
-		for k, v in ontology[facet_key].items():
-			if k not in langs and k!='sortid':
-				if facet_key == 'default':
-					v  = v[lang]
-				else:
-					try:
-						# Attempt to translate the facet key using the ontology dictionary.
-						facet_key_lang = ontology[facet_key].get(lang, facet_key)
-					except:
-						facet_key_lang = facet_key
-					v  = '%s &#x25BA; %s'%(facet_key_lang, v[lang])
-				attropts.append([k, v])
+	for facet in list(ontology.keys()):
+		for term in list(ontology[facet].keys()):
+			attropts.append([term, ontology[facet][term]])
 
 	return attropts
 

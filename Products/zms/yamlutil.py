262c61e4e76f90cc538c590eb6af98b8bb56e366
zmsdev <zmsdeveloper@gmail.com>
2025-08-28 21:15:10 +0200

    fix tests

--- Products/zms/yamlutil.py
+++ Products/zms/yamlutil.py
@@ -16,15 +16,5 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 ################################################################################
 
-import json
-import re
-
 def parse_yaml(data):
-    # Convert YAML-like data to JSON-like data
-    def yaml_to_json(data):
-        data = re.sub(r'(?m)^(\s*)([^:\s]+):', r'\1"\2":', data)  # Quote keys
-        data = re.sub(r'(?m):\s*([^,\s]+)', r': "\1"', data)       # Quote values
-        return data
-
-    json_data = yaml_to_json(data)
-    return json.loads(json_data)
+    return {}

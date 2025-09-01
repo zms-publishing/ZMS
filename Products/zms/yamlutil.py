f61518523d0821898780685e06acaab5acf9b33a
zmsdev <zmsdeveloper@gmail.com>
2025-09-01 09:51:23 +0200

    cleanup lists and dicts in yaml

--- Products/zms/yamlutil.py
+++ Products/zms/yamlutil.py
@@ -16,10 +16,41 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 ################################################################################
 
-def dump(data):
+
+def dump(data, clean=True):
     from yaml import dump
+    if clean:
+        data = __cleanup(data)
     return dump(data)
 
 def parse(data):
     from yaml import safe_load
     return safe_load(data)
+
+def __cleanup(v):
+    """
+    Recursively cleans up a dictionary by removing keys with falsy values.
+
+    Args:
+        v (dict or any): The input value, which can be a dictionary or any other type.
+
+    Returns:
+        dict or any: If the input is a dictionary, returns a new dictionary with keys
+        whose values are falsy removed. If the input is not a dictionary, returns the
+        input value unchanged.
+    """
+    if isinstance(v, dict):
+        nd = {}
+        for k in list(v.keys()):
+            nv = __cleanup(v[k])
+            if nv:
+                nd[k] = nv
+        return nd
+    elif isinstance(v, list):
+        nl = []
+        for i in v:
+            nv = __cleanup(i)
+            if nv:
+                nl.append(nv)
+        return nl
+    return v

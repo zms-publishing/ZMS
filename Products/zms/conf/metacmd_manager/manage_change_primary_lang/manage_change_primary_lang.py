88644b763834703757aa138ecea4dd7cd01020ab
zmsdev <zmsdeveloper@gmail.com>
2025-10-23 10:55:07 +0200

    prepare swap content

--- Products/zms/conf/metacmd_manager/manage_change_primary_lang/manage_change_primary_lang.py
+++ Products/zms/conf/metacmd_manager/manage_change_primary_lang/manage_change_primary_lang.py
@@ -11,14 +11,14 @@ from Products.zms import standard
 request = container.REQUEST
 RESPONSE =  request.RESPONSE
 
-def traverse(node, prim_lang, new_prim_lang):
+def traverse(node, prim_lang, new_prim_lang, swap_content=False):
   c = 1
   coverage = node.attr('attr_dc_coverage') 
   standard.writeLog(node,'coverage=%s'%str(coverage))
   if coverage == 'global.%s'%prim_lang:
     node.attr('attr_dc_coverage','global.%s'%new_prim_lang)
   for child_node in node.getChildNodes():
-    c += traverse(child_node, prim_lang, new_prim_lang)
+    c += traverse(child_node, prim_lang, new_prim_lang, swap_content)
   if node.meta_id == 'ZMS':
     standard.writeLog(node,'Change primary language: %s -> %s'%(prim_lang,new_prim_lang))
     langs = context.getLangs()
@@ -26,7 +26,7 @@ def traverse(node, prim_lang, new_prim_lang):
     context.setLanguage(new_prim_lang, langs[new_prim_lang]['label'], '', langs[new_prim_lang]['manage'])
     for portal_client in node.getPortalClients():
       if portal_client is not None:
-        c += traverse(portal_client, prim_lang, new_prim_lang)
+        c += traverse(portal_client, prim_lang, new_prim_lang, swap_content)
   return c
 
 print('<!DOCTYPE html>')
@@ -46,7 +46,9 @@ print('<div class="card-body">')
 # ---------------------------------
 if request.form.get('btn')=='BTN_EXECUTE':
     message = []
-    c = traverse(context, context.getPrimaryLanguage(), request.form.get('new_prim_lang'))
+    new_prim_lang = request.form.get('new_prim_lang','').strip()
+    swap_content = bool(int(request.form.get('swap_content','0')))
+    c = traverse(context, context.getPrimaryLanguage(), new_prim_lang, swap_content)
     message.append('%s: %i'%(context.getZMILangStr('MSG_CHANGED'),c))
     return request.response.redirect(context.url_append_params('%s/manage_main'%context.absolute_url(),{'lang':request['lang'],'manage_tabs_message':'<br/>'.join(message)}))
 
@@ -65,6 +67,12 @@ else:
 			selected = (lang_id == context.getPrimaryLanguage()) and 'selected' or ''
 			print('<option value="%s">%s -&gt; %s (%s)</option>'%(lang_id,context.getPrimaryLanguage(),lang_id,context.getLanguageLabel(lang_id)))
 	print('</select>') 
+	print('</div><!-- .col-sm-10 -->')
+	print('</div><!-- .form-group -->')
+	print('<div class="form-group row">')
+	print('<div class="col-sm-2 control-label">Swap content</div>')
+	print('<div class="col-sm-10">')
+	print('<input type="checkbox" class="form-check-input ml-0" id="swap_content" name="swap_content" value="1"/>')
 	print('</div><!-- .col-sm-10 -->')
 	print('</div><!-- .form-group -->')
 	print('<div class="form-group row">')

dec9c05deff2db712310efc5e63d85c27c0a2900
drfho <dr.frank.hoffmann@web.de>
2026-02-18 12:26:32 +0100

    changed linkvalidation-default on property-processing to false
    Ref:
    1. https://github.com/zms-publishing/ZMS/pull/459
    2. https://github.com/zms-publishing/ZMS/pull/459/changes/ca3b92267f2fe56d0492aa51b03b7e7cd24ce3ef
    3. https://github.com/idasm-unibe-ch/unibe-cms/issues/1124

--- Products/zms/_objattrs.py
+++ Products/zms/_objattrs.py
@@ -571,10 +571,10 @@ class ObjAttrs(object):
         value = self.getObjAttrValue( obj_attr, REQUEST)
         if value:
           # Text-Fields
-          if datatype in _globals.DT_TEXTS:
+          if datatype in _globals.DT_TEXTS and bool(self.getConfProperty('ZReferableItem.validateInlineLinkObj', 0)):
             value = self.validateInlineLinkObj(value)
           # Url-Fields
-          if datatype == _globals.DT_URL:
+          if datatype == _globals.DT_URL and bool(self.getConfProperty('ZReferableItem.validateLinkObj', 0)):
             value = self.validateLinkObj(value)
           # Executable fields.
           value = standard.dt_exec(self, value)

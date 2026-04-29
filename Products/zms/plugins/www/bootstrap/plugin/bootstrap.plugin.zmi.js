cf3f1a8c5842337728532a1755f1746c2933b63e
drfho <dr.frank.hoffmann@web.de>
2026-04-30 01:12:33 +0800

    zmi(js): fix mandatory check

--- Products/zms/plugins/www/bootstrap/plugin/bootstrap.plugin.zmi.js
+++ Products/zms/plugins/www/bootstrap/plugin/bootstrap.plugin.zmi.js
@@ -733,7 +733,7 @@ ZMI.prototype.initInputFields = function(container) {
 					else if (nodeName=="select") {
 						isBlank =  (($("option:selected",$control).length==0)
 							|| ($("option:selected",$control).length==1 && $("option:selected",$control).attr("value")==""))
-							&& !$control.attr("disabled")=="disabled";
+							&& $control.attr("disabled")!="disabled";
 					}
 					if (isBlank) {
 						$controlGroup.addClass("has-error");
@@ -749,7 +749,7 @@ ZMI.prototype.initInputFields = function(container) {
 						if (typeof dataExclude != "undefined") {
 							var excludeList = dataExclude.split(",");
 							var v = $control.val();
-							if ($.inArray(v,excludeList)) {
+							if ($.inArray(v,excludeList) !== -1) {
 								$controlGroup.addClass("has-error");
 							}
 						}

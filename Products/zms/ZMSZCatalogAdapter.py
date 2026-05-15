a567f3cafdf3dbbdc2c11c2bb5c0612352a452b1
drfho <dr.frank.hoffmann@web.de>
2026-05-16 00:41:14 +0200

    refact search-indexed sortid -value (up to 5 levels, 15 digits)
    Ref: https://github.com/idasm-unibe-ch/unibe-cms/issues/1244

--- Products/zms/ZMSZCatalogAdapter.py
+++ Products/zms/ZMSZCatalogAdapter.py
@@ -43,14 +43,9 @@ def get_default_data(node):
   d['change_dt'] = get_zoned_dt(node.attr('change_dt')) or d['created_dt']
   d['indexing_dt'] = get_zoned_dt(time.gmtime())
   # WIP: https://github.com/idasm-unibe-ch/unibe-cms/issues/1244
-  parent_node = node.getParentNode()
-  if parent_node:
-    if parent_node == node:
-      parent_node = parent_node.getParentNode()
-    parent_sortid = parent_node.getSortId()
-  else:
-    parent_sortid = 0
-  d['sortid'] = '%03i%03i%03i'%(node.getLevel(), parent_sortid, node.getSortId() or 0)
+  # sortid is composed of sort-ids (3 digits each) up to 5 levels.
+  sortid = ''.join(['%03i'%(e.getSortId()) for e in node.breadcrumbs_obj_path(False)[1:]])
+  d['sortid'] = sortid + ('0' * (15 - len(sortid)))
   return d
 
 def get_zoned_dt(struct_dt):

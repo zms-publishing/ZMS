8836d7584b40bdc2cdc19922fc584319bdc88e0b
drfho <dr.frank.hoffmann@web.de>
2026-09-05 22:57:42 +0200

    zmsqldb: allow Zope variable $INSTANCE_HOME for blob paths
    Ref: https://github.com/sntl-projects/zhref/issues/22#issuecomment-5552134049

--- Products/zms/zmssqldb.py
+++ Products/zms/zmssqldb.py
@@ -1988,6 +1988,7 @@ class ZMSSqlDb(zmscustom.ZMSCustom):
       column = self.getEntityColumn( tablename, id)
       blob = column['blob']
       path = blob['path']
+      path = path.replace('$INSTANCE_HOME', standard.getINSTANCE_HOME())
       # Assemble sql-statement
       sqlStatement = []
       sqlStatement.append( 'SELECT '+id+' AS v FROM %s '%tablename)
@@ -2058,6 +2059,7 @@ class ZMSSqlDb(zmscustom.ZMSCustom):
       column = self.getEntityColumn( tablename, id)
       blob = column['blob']
       path = blob['path']
+      path = path.replace('$INSTANCE_HOME', standard.getINSTANCE_HOME())
       if file is None and xml is not None:
         file = standard.parseXmlString( xml)
       # Normalize filename (crop path in local-fs)
@@ -2147,6 +2149,7 @@ class ZMSSqlDb(zmscustom.ZMSCustom):
       column = self.getEntityColumn( tablename, id)
       blob = column['blob']
       path = blob['path']
+      path = path.replace('$INSTANCE_HOME', standard.getINSTANCE_HOME())
       # Assemble sql-statement
       sqlStatement = []
       sqlStatement.append( 'SELECT '+id+' AS v FROM %s '%tablename)
